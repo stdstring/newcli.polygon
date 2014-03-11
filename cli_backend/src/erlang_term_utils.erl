93bc9ecba8076ac2498b83d693a00c4ff741148c
Ushakov Andrey <std_string@mail.ru>
2014-03-11 15:05:42 +0600

    Now we can write term to file.

--- cli_backend/src/erlang_term_utils.erl
+++ cli_backend/src/erlang_term_utils.erl
@@ -7,10 +7,16 @@
 %% ====================================================================
 %% API functions
 %% ====================================================================
--export([read_from_file/1]).
+-export([read_from_file/1, write_to_file/2, append_to_file/2]).
 
 read_from_file(Filename) ->
     read_from_file_impl(file:open(Filename, [read])).
+
+write_to_file(Filename, Term) ->
+    write_to_file_impl(file:open(Filename, [write]), Term).
+
+append_to_file(Filename, Term) ->
+    write_to_file_impl(file:open(Filename, [append]), Term).
 
 %% ====================================================================
 %% Internal functions
@@ -22,6 +28,13 @@ read_from_file_impl({ok, IoDevice}) ->
         {ok, Term} -> Term;
         eof -> error({file_read_error, eof});
         {error, Reason} -> error({file_read_error, Reason})
+    after
+        file:close(IoDevice)
+    end.
+
+write_to_file_impl({error, Reason}, _Term) -> error({file_write_error, Reason});
+write_to_file_impl({ok, IoDevice}, Term) ->
+    try io:write(IoDevice, Term)
     after
         file:close(IoDevice)
     end.
