00fee2f9ed8ac4c2ef2e9675109806fc6d6dbdf1
Ushakov Andrey <std_string@mail.ru>
2014-03-11 18:51:55 +0600

    Add additional callbacks into command's behaviour

--- cli_backend/src/command_behaviour.erl
+++ cli_backend/src/command_behaviour.erl
@@ -2,11 +2,17 @@
 
 -module(command_behaviour).
 
+%% get command name
+-callback get_name() -> Name :: atom().
+
+%% get command body
+-callback get_command_body() -> [string()].
+
 %% get command help
 -callback get_help() -> Help :: string().
 
 %% create command's instance
--callback create(CommandLineRest :: string()) -> CommandPid :: pid().
+-callback create(CommandLineRest :: string()) -> Command :: pid().
 
 %% synchronous executing
 -callback execute(Command :: pid(), Stdout :: pid(), Stderr :: pid()) -> ReturnCode :: integer().
