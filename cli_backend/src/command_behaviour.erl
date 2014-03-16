590ff7925b6f12ec1fdd458603f1709785c2e543
Ushakov Andrey <std_string@mail.ru>
2014-03-16 11:44:06 +0600

    rename parameter in callback

--- cli_backend/src/command_behaviour.erl
+++ cli_backend/src/command_behaviour.erl
@@ -12,7 +12,7 @@
 -callback get_help() -> Help :: string().
 
 %% create command's instance
--callback create(CommandLineRest :: string()) -> Command :: pid().
+-callback create(CommandLine :: string()) -> Command :: pid().
 
 %% synchronous executing
 -callback execute(Command :: pid(), Stdout :: pid(), Stderr :: pid()) -> ReturnCode :: integer().
