9cb06d9c395ec73dbf874ddbbf88cb0dec50c12b
Ushakov Andrey <std_string@mail.ru>
2014-03-24 16:36:12 +0600

    no switchport vlan command

--- cli_backend/src/commands/no_switchport_vlan_command.erl
+++ cli_backend/src/commands/no_switchport_vlan_command.erl
@@ -5,6 +5,10 @@
 -behaviour(command_behaviour).
 -behaviour(gen_server).
 
+-include("command_defs.hrl").
+
+-define(COMMAND, no_switchport_vlan_command).
+
 %% ====================================================================
 %% API functions
 %% ====================================================================
@@ -19,17 +23,25 @@ get_command_body() -> ["no", "switchport", "access", "vlan"].
 
 get_help() -> "no switchport access vlan command".
 
-create(CommandLineRest, Stdout, Stderr) -> error(not_implemented).
+create(CommandLineRest, Stdout, Stderr) ->
+    case check_command(CommandLineRest) of
+        false -> {?COMMAND, bad_args};
+        true -> start_command(Stdout, Stderr)
+    end.
 
-execute(Command) -> error(not_implemented).
+execute(Command) ->
+    gen_server:call(Command, execute).
 
-init(_Args) -> error(not_implemented).
+init({Stdout, Stderr}) ->
+    State = #command_state{command_line_rest = "", stdout = Stdout, stderr = Stderr},
+    {ok, State}.
 
-handle_call(_Request, _From, _State) -> error(not_implemented).
+handle_call(execute, _From, State) ->
+    {reply, 0, State}.
 
-handle_cast(_Request, _State) -> error(not_implemented).
+handle_cast(_Request, _State) -> error(not_supported).
 
-handle_info(_Request, _Info) -> error(not_supported).
+handle_info(_Info, State) -> {noreply, State}.
 
 terminate(_Reason, _State) -> true.
 
@@ -40,3 +52,13 @@ code_change(_OldVsn, State, _Extra) -> {ok, State}.
 %% Internal functions
 %% ====================================================================
 
+-spec check_command(CommandLineRest :: string()) -> boolean().
+check_command(CommandLineRest) ->
+    CommandLineRest == "".
+
+-spec start_command(Stdout :: pid(), Stderr  :: pid()) -> pid() | {'no_switchport_vlan_command', Error :: term()}.
+start_command(Stdout, Stderr) ->
+    case gen_server:start_link(?MODULE, {Stdout, Stderr}, []) of
+        {ok, Pid} -> Pid;
+        {error, Error} -> {?COMMAND, Error}
+    end.
