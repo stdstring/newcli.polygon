80cd796fb2d3819f6fe3a12d91366270d8cd8eb7
Ushakov Andrey <std_string@mail.ru>
2014-03-26 00:02:06 +0600

    new cmmand parser based on fsm

--- cli_backend/src/command_parser.erl
+++ cli_backend/src/command_parser.erl
@@ -10,12 +10,13 @@
 
 -export([parse/3]).
 
--spec parse(CommandLine :: string(), Config :: #config{}, ClientOutput :: pid()) -> [pid()] | 'false'.
+-spec parse(CommandLine :: string(), Config :: #config{}, ClientOutput :: pid()) ->
+          [{ModuleName :: atom(), CommandPid :: pid()}] | {'command_parser', Reason :: term()}.
 parse(CommandLine, Config, ClientOutput) when is_record(Config, config) ->
     Commands = Config#config.commands,
     case find_command(CommandLine, Commands) of
         {_, CommandModule, CommandLineRest} -> create_command_chain(CommandModule, CommandLineRest, ClientOutput);
-        false -> {command_parser, unknown_command}
+        {false, Reason} -> {command_parser, Reason}
     end.
 
 %% ====================================================================
@@ -23,66 +24,36 @@ parse(CommandLine, Config, ClientOutput) when is_record(Config, config) ->
 %% ====================================================================
 
 -spec find_command(CommandLine :: string(), Commands :: [{CommandName :: atom(), CommandModule :: atom()}]) ->
-          {CommandName :: atom(), CommandModule :: atom(), ComandLineRest :: string()} | 'false'.
+          {CommandName :: atom(), CommandModule :: atom(), ComandLineRest :: string()} | {'false', Reason :: term()}.
 find_command(CommandLine, Commands) ->
-    {[FirstToken], Rest} = commandline_parser:get_tokens(string:strip(CommandLine), 1),
-    case find_command(FirstToken, Rest, Commands, undefined) of
-        {Name, Module, _TokensCount, OtherRest} -> {Name, Module, OtherRest};
-        false -> false
+    case command_parser_fsm:start(Commands) of
+        {command_parser_fsm, Error} -> {false, {command_parser_fsm, Error}};
+        CommandParserFsm -> find_command(CommandParserFsm, string:strip(CommandLine), undefined)
     end.
 
--spec find_command(FirstToken :: string(),
-                   Rest :: string(),
-                   Commands :: [{CommandName :: atom(), CommandModule :: atom()}],
-                   BestCommand :: {Name :: atom(), Module  :: atom(), TokensCount :: integer(), OtherRest :: string()} | 'undefined') ->
-          {CommandName :: atom(), CommandModule :: atom(), ComandLineRest :: string()} | 'false'.
-find_command(_FirstToken, _Rest, [], undefined) -> false;
-find_command(_FirstToken, _Rest, [], BestCommand) -> BestCommand;
-find_command(FirstToken, Rest, Commands, undefined) ->
-    [{_Name, Module} | _CommandsRest] = Commands,
-    CommandBody = apply(Module, get_command_body, []),
-    apply_check(FirstToken, Rest, CommandBody, Commands, undefined);
-find_command(FirstToken, Rest, Commands, BestCommand) ->
-    [{_Name, Module} | _CommandsRest] = Commands,
-    {_Name, _Module, BestCount, _OtherRest} = BestCommand,
-    CommandBody = apply(Module, get_command_body, []),
-    CommandBodyLength = length(CommandBody),
-    if
-        CommandBodyLength > BestCount -> apply_check(FirstToken, Rest, CommandBody, Commands, BestCommand);
-        CommandBodyLength =< BestCount -> find_command(FirstToken, Rest, Commands, BestCommand)
-    end.
+-spec find_command(CommandParserFsm :: pid(), Rest :: string(), Result :: term()) ->
+          {CommandName :: atom(), CommandModule :: atom(), ComandLineRest :: string()} | {'false', Reason :: term()}.
+find_command(_CommandParserFsm, Rest, {successful_parsing, {Name, Module}}) -> {Name, Module, Rest};
+find_command(_CommandParserFsm, _Rest, unsuccessful_parsing) -> {false, unknown_command};
+find_command(_CommandParserFsm, "", ambiguous_parsing) -> {false, ambiguous_command};
+find_command(_CommandParserFsm, "", incomplete_parsing) -> {false, incomplete_command};
+find_command(CommandParserFsm, Rest, _Result) ->
+    {Token, NewRest} = commandline_parser:get_first_token(Rest),
+    Result = command_parser_fsm:process_token(CommandParserFsm, Token),
+    find_command(CommandParserFsm, NewRest, Result).
 
--spec apply_check(FirstToken :: string(),
-                  Rest :: string(),
-                  CommandBody :: [string()],
-                  Commands :: [{CommandName :: atom(), CommandModule :: atom()}],
-                  OldBestCommand :: {Name :: atom(), Module  :: atom(), TokensCount :: integer(), OtherRest :: string()} | 'undefined') -> no_return().
-apply_check(FirstToken, Rest, CommandBody, Commands, OldBestCommand) ->
-    [{Name, Module} | CommandsRest] = Commands,
-    case check_command(FirstToken, Rest, CommandBody) of
-        {true, TokensCount, OtherRest} -> find_command(FirstToken, Rest, CommandsRest, {Name, Module, TokensCount, OtherRest});
-        false -> find_command(FirstToken, Rest, CommandsRest, OldBestCommand)
-    end.
-
--spec check_command(FirstToken :: string(), Rest :: string(), Body :: [string()]) -> {'true', TokenCount :: integer, OtherRest :: string()} | 'false'.
-check_command(FirstToken, _Rest, [FirstWord | _Words]) when FirstToken /= FirstWord -> false;
-check_command(FirstToken, Rest, Body) ->
-    [FirstToken | Words] = Body,
-    {OtherTokens, OtherRest} = commandline_parser:get_tokens(Rest, length(Words)),
-    Tokens = [FirstToken] ++ OtherTokens,
-    logic_utils:ternary_op(Tokens == Body, {true, length(Tokens), OtherRest}, false).
-
--spec create_command_chain(CommandModule :: atom(), CommandLineRest :: string(), ClientOutput :: pid()) -> [pid()].
+-spec create_command_chain(CommandModule :: atom(), CommandLineRest :: string(), ClientOutput :: pid()) ->
+          [{ModuleName :: atom(), CommandPid :: pid()}].
 create_command_chain(CommandModule, CommandLineRest, ClientOutput) ->
     case output_endpoint:start(ClientOutput) of
-        {output_endpoint, Error} -> {output_endpoint, Error};
+        {output_endpoint, Error} -> {command_parser, {output_endpoint, Error}};
         OutputEndpointPid ->
             Stdout = OutputEndpointPid,
             Stderr = OutputEndpointPid,
             case apply(CommandModule, create, [CommandLineRest, Stdout, Stderr]) of
                 {error, Reason} ->
                     CommandName = apply(CommandModule, get_name, []),
-                    {CommandName, {creation_error, Reason}};
-                CommandPid -> [CommandPid, OutputEndpointPid]
+                    {command_parser, {CommandName, creation_error, Reason}};
+                CommandPid -> [{CommandModule, CommandPid}, {output_endpoint, OutputEndpointPid}]
             end
     end.
--- cli_backend/src/commandline_parser.erl
+++ cli_backend/src/commandline_parser.erl
@@ -6,28 +6,14 @@
 %% API functions
 %% ====================================================================
 
--export([get_first_token/1, get_tokens/2, get_tokens/3]).
+-export([get_first_token/1]).
 
 -spec get_first_token(Source :: string()) -> {Tokens :: [string()], Rest :: string()}.
 get_first_token(Source) ->
-        get_tokens(Source, 1).
-
--spec get_tokens(Source :: string(), Count :: integer()) -> {Tokens :: [string()], Rest :: string()}.
-get_tokens(Source, Count) ->
     %% 32 == space ascii code
-    get_tokens(Source, 32, Count).
-
--spec get_tokens(Source :: string(), Delimiter :: integer(), Count :: integer()) -> {Tokens :: [string()], Rest :: string()}.
-get_tokens(Source, Delimiter, Count) ->
-    get_tokens_impl(Source, Delimiter, Count, []).
+    lists:splitwith(fun(Character) -> Character /= 32 end, Source).
 
 %% ====================================================================
 %% Internal functions
 %% ====================================================================
 
--spec get_tokens_impl(Source :: string(), Delimiter :: char(), Count :: integer(), Tokens :: [string()]) -> {Tokens :: [string()], Rest :: string()}.
-get_tokens_impl(Source, _Delimiter, 0, Tokens) -> {lists:reverse(Tokens), Source};
-get_tokens_impl("", _Delimiter, _Count, Tokens) -> {lists:reverse(Tokens), ""};
-get_tokens_impl(Source, Delimiter, Count, Tokens) ->
-    {Token, Rest} = lists:splitwith(fun(Character) -> Character /= Delimiter end, Source),
-    get_tokens_impl(string:strip(Rest, left, Delimiter), Delimiter, Count - 1, [Token] ++ Tokens).
