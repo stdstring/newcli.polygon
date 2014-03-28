4cae277c82a3a05026220baec2253fbfa81b47dd
Ushakov Andrey <std_string@mail.ru>
2014-03-28 23:11:54 +0600

    improvements of command_execution_checker

--- cli_backend/src/command_execution_checker.erl
+++ cli_backend/src/command_execution_checker.erl
@@ -8,17 +8,27 @@
 %% API functions
 %% ====================================================================
 
--export([execution_precheck/3]).
+-export([execution_precheck/3, execution_postcheck/1]).
 
--spec execution_precheck(CommandModule :: atom(), CliFsm :: pid(), User :: #user{}) -> boolean().
+-spec execution_precheck(CommandModule :: atom(), CliFsm :: pid(), User :: #user{}) -> 'true' | {'false', Reason :: atom()}.
 execution_precheck(CommandModule, CliFsm, User) ->
     CommandName = apply(CommandModule, get_name, []),
     case authorization_service:authorize(User, CommandName) of
         {authorization_result, access_allowed} ->
             {_CurrentState, Commands} = cli_fsm:get_current_state(CliFsm),
-            lists:member(CommandName, Commands);
-        {authorization_result, access_denied} -> false;
-        {authorization_fail, unknown_command} -> false
+            case lists:member(CommandName, Commands) of
+                true -> true;
+                false -> {false, unsuitable_command}
+            end;
+        {authorization_result, access_denied} -> {false, access_denied};
+        {authorization_fail, unknown_command} -> {false, authorization_bad_config}
+    end.
+
+execution_postcheck(CliFsm) ->
+    #cli_fsm_state_info{is_terminal =IsTerminalState} = cli_fsm:get_current_state(CliFsm),
+    case IsTerminalState of
+        false -> true;
+        true -> {false, cli_terminal_state}
     end.
 
 %% ====================================================================
