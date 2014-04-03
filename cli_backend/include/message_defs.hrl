411d54fd4d24b9a71e77d87c2d22b6cd8b9b02a2
Ushakov Andrey <std_string@mail.ru>
2014-04-03 15:33:14 +0600

    change messages and create records for messages

--- cli_backend/include/message_defs.hrl
+++ cli_backend/include/message_defs.hrl
@@ -1,7 +1,16 @@
 %% message definitions:
 
 -record(login, {login_name = "" :: string(), password = <<>> :: binary()}).
+-record(login_success, {session_pid = undefined :: 'undefined' | pid(), greeting = "" :: string()}).
+-record(login_fail, {reason = undefined :: 'undefined' | term()}).
+
 -record(logout, {}).
+-record(logout_success, {}).
+
+-record(commands_info, {}).
+-record(command_info, {command_name :: atom(), command_body :: [string()], command_help :: string()}).
+-record(commands_info_result, {info :: [#command_info{}]}).
+-record(commands_info_fail, {reason = undefined :: 'undefined' | term()}).
 
 -record(command, {message = "" :: string()}).
 -record(command_output, {message = "" :: string()}).
