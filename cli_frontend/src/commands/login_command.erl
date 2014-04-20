1a1a57f7706f641ecc59d56409c909442c530adb
Ushakov Andrey <std_string@mail.ru>
2014-04-20 15:04:49 +0600

    login command implementation. intermediate result

--- cli_frontend/src/commands/login_command.erl
+++ cli_frontend/src/commands/login_command.erl
@@ -2,6 +2,7 @@
 
 -module(login_command).
 
+-include("message_defs.hrl").
 -include("common_defs.hrl").
 
 %% ====================================================================
@@ -20,7 +21,29 @@ get_command_body() -> ["login"].
 %%create(CommandLineRest) -> {error, not_implemented}.
 
 -spec execute(CommandLineRest :: string(), ExecutionState :: #execution_state{}) -> {ReturnCode :: integer(), ExecutionState :: #execution_state{}}.
-execute(_CommandLineRest, ExecutionState) -> {0, ExecutionState}.
+execute("", ExecutionState) ->
+    OldOptions = io:getopts(),
+    OptionsWithoutExpand = lists:keydelete(expand_fun, 1, OldOptions),
+    EchoOnOptions = lists:keystore(echo, 1, OptionsWithoutExpand, {echo, true}),
+    EchoOffOptions = lists:keystore(echo, 1, OptionsWithoutExpand, {echo, false}),
+    io:setopts(EchoOnOptions),
+    LoginLine = io:get_line("login:"),
+    io:setopts(EchoOffOptions),
+    PwdLine = io:get_line("password:"),
+    io:setopts(OldOptions),
+    Login = string:strip(LoginLine, right, $\n),
+    PwdString = string:strip(PwdLine, right, $\n),
+    Pwd = <<>>,
+    LoginCommand = #login{login_name = Login, password = Pwd},
+    case gen_server:call(global_input_endpoint, LoginCommand) of
+        #login_success{session_pid = Session, greeting = Greeting} ->
+            io:format(Greeting, []),
+            NewExecutionState = ExecutionState#execution_state{session = Session},
+            {0, NewExecutionState};
+        #login_fail{reason = Reason} ->
+            io:format(standard_error, "Login's attempt is failed due to the following: ~p~n", [Reason]),
+            {255, ExecutionState}
+    end.
 
 %% ====================================================================
 %% Internal functions
