bb8dedd73b2d4aa8518e4e2e339ec84f42a80bab
Ushakov Andrey <std_string@mail.ru>
2014-04-21 23:03:58 +0600

    help command implementation

--- cli_frontend/src/commands/help_command.erl
+++ cli_frontend/src/commands/help_command.erl
@@ -20,7 +20,22 @@ get_command_body() -> ["?"].
 %% create(CommandLineRest) -> {error, not_implemented}.
 
 -spec execute(CommandLineRest :: string(), ExecutionState :: #execution_state{}) -> {ReturnCode :: integer(), ExecutionState :: #execution_state{}}.
-execute(_CommandLineRest, ExecutionState) -> {0, ExecutionState}.
+execute(CommandLineRest, ExecutionState) ->
+    SourceCommands = ExecutionState#execution_state.commands_info,
+    HasTrailingSpace = lists:suffix(" ", CommandLineRest),
+    if
+        CommandLineRest == "" ->
+            show_commands(SourceCommands);
+        HasTrailingSpace == false ->
+            Tokens = commandline_parser:get_tokens(CommandLineRest),
+            FilteredCommands = filter_commands(SourceCommands, Tokens),
+            show_commands(FilteredCommands);
+        HasTrailingSpace == true ->
+            Tokens = commandline_parser:get_tokens(CommandLineRest),
+            Command = find_command(SourceCommands, Tokens),
+            show_command(Command)
+    end,
+    {0, ExecutionState}.
 
 %% ====================================================================
 %% Internal functions
@@ -34,11 +49,38 @@ find_command(Commands, CommandPrefix) ->
         [] -> false
     end.
 
-%%filter_commands(Commands, CommandPrefix) ->
-%%    {FullParts, PartialPart} = lists:split(length(CommandPrefix) - 1, CommandPrefix),
-%%    PartialPartIndex = length(CommandPrefix),
-%%    lists:filter(fun({_Name, Body, _Help}) -> lists end, Commands)
+-spec filter_command(CommandBody :: [string()], Prefix :: [string()]) -> boolean().
+filter_command([], _PrefixRest) -> false;
+filter_command([CommandBodyPart | _CommandBodyRest], [PartialPrefixPart]) ->
+    lists:prefix(PartialPrefixPart, CommandBodyPart);
+filter_command([CommandBodyPart | CommandBodyRest], [PrefixPart | PrefixRest]) ->
+    case CommandBodyPart == PrefixPart of
+        true -> filter_command(CommandBodyRest, PrefixRest);
+        false -> false
+    end.
 
-get_command_body(CommandBodyParts) ->
+-spec filter_commands(Commands :: [{CommandName :: atom(), CommandBody :: [string()], CommandHelp :: string()}], CommandPrefix :: [string()]) ->
+          [{CommandName :: atom(), CommandBody :: [string()], CommandHelp :: string()}].
+filter_commands(Commands, CommandPrefix) ->
+    lists:filter(fun({_Name, Body, _Help}) -> filter_command(Body, CommandPrefix) end, Commands).
+
+join_command_body(CommandBodyParts) ->
     %% separator == space
     string:join(CommandBodyParts, " ").
+
+-spec show_commands(Commands :: [{CommandName :: atom(), CommandBody :: [string()], CommandHelp :: string()}]) -> 'ok'.
+show_commands([]) ->
+    io:format("Commands not found~n"),
+    ok;
+show_commands(Commands) ->
+    CommandBodyList = lists:map(fun({_Name, Body, _Help}) -> join_command_body(Body) end, Commands),
+    io:format("~s~n", [string:join(CommandBodyList, "\t")]),
+    ok.
+
+-spec show_command(Command :: {CommandName :: atom(), CommandBody :: [string()], CommandHelp :: string()} | 'false') -> 'ok'.
+show_command(false) ->
+    io:format("Help not found~n"),
+    ok;
+show_command({_Name, _Body, Help}) ->
+    io:format("~s~n", [Help]),
+    ok.
