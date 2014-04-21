1728a9be1f0d01cd6d4eb6bb8b52473e1ebfb3b1
Ushakov Andrey <std_string@mail.ru>
2014-04-21 17:03:34 +0600

    some additions into help_command

--- cli_frontend/src/commands/help_command.erl
+++ cli_frontend/src/commands/help_command.erl
@@ -22,10 +22,22 @@ get_command_body() -> ["?"].
 -spec execute(CommandLineRest :: string(), ExecutionState :: #execution_state{}) -> {ReturnCode :: integer(), ExecutionState :: #execution_state{}}.
 execute(_CommandLineRest, ExecutionState) -> {0, ExecutionState}.
 
-
 %% ====================================================================
 %% Internal functions
 %% ====================================================================
+
+-spec find_command(Commands :: [{CommandName :: atom(), CommandBody :: [string()], CommandHelp :: string()}], CommandPrefix :: [string()]) ->
+          {CommandName :: atom(), CommandBody :: [string()], CommandHelp :: string()} | 'false'.
+find_command(Commands, CommandPrefix) ->
+    case lists:filter(fun({_Name, Body, _Help}) -> CommandPrefix == Body end, Commands) of
+        [{CommandName, CommandBody, CommandHelp}] -> {CommandName, CommandBody, CommandHelp};
+        [] -> false
+    end.
+
+%%filter_commands(Commands, CommandPrefix) ->
+%%    {FullParts, PartialPart} = lists:split(length(CommandPrefix) - 1, CommandPrefix),
+%%    PartialPartIndex = length(CommandPrefix),
+%%    lists:filter(fun({_Name, Body, _Help}) -> lists end, Commands)
 
 get_command_body(CommandBodyParts) ->
     %% separator == space
