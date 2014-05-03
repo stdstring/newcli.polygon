226d7d0324d0c42735b8cd7728249e4fceb63a63
Ushakov Andrey <std_string@mail.ru>
2014-05-03 10:40:20 +0600

    debug autocomplete

--- cli_frontend/src/autocomplete_factory.erl
+++ cli_frontend/src/autocomplete_factory.erl
@@ -15,7 +15,8 @@
 create_expand_fun(ExecutionState) ->
     CommandsInfo = ExecutionState#execution_state.commands_info,
     AccFun = fun({_Name, Body, _Help}, Storage) -> process_command(join_command_body(Body), Storage) end,
-    ResultStorage = lists:foldl(AccFun, dict:new(), CommandsInfo),
+    ResultStorage = post_process_commands(lists:foldl(AccFun, dict:new(), CommandsInfo)),
+    io:format("~p~n", [dict:to_list(ResultStorage)]),
     fun(Input) ->
             PreparedInput = extra_spaces_cleanup(Input),
             case dict:is_key(PreparedInput, ResultStorage) of
@@ -32,39 +33,50 @@ create_expand_fun(ExecutionState) ->
 
 -spec process_command(Body :: string(), Storage :: dict()) -> dict().
 process_command(Body, Storage) ->
-    process_command(Body, "", Storage).
+    process_command(Body, Body, undefined, Storage).
 
--spec process_command(BodyRest :: string(), Key :: string(), Storage :: dict()) -> dict().
-process_command("", _Key, Storage) -> Storage;
-process_command([Header | BodyRest], Key, Storage) ->
+-spec process_command(BodySource :: string(), BodyRest :: string(), Key :: string(), Storage :: dict()) -> dict().
+process_command(_BodySource, "", _Key, Storage) -> Storage;
+process_command(BodySource, BodyRest, undefined, Storage) ->
+    NewKey = "",
+    NewStorage = process_body(BodySource, BodyRest, NewKey, Storage),
+    process_command(BodySource, BodyRest, NewKey, NewStorage);
+process_command(BodySource, [Header | BodyRest], Key, Storage) ->
     NewKey = [Header] ++ Key,
-    process_command_body(BodyRest, NewKey, Storage).
+    NewStorage = process_body(BodySource, BodyRest, NewKey, Storage),
+    process_command(BodySource, BodyRest, NewKey, NewStorage).
 
--spec process_command_body(BodyRest :: string(), Key :: string(), Storage :: dict()) -> dict().
-process_command_body(BodyRest, Key, Storage) ->
-    InitialValue = {BodyRest, []},
+-spec process_body(BodySource :: string(), BodyRest :: string(), Key :: string(), Storage :: dict()) -> dict().
+process_body(BodySource, BodyRest, Key, Storage) ->
+    InitialValue = {BodyRest, [BodySource]},
     UpdateFun = fun({OldCommonPart, OldCaseList}) ->
-                        {CommonPart, OldRest, NewRest} = calculate_common_part(OldCommonPart, BodyRest),
-                        NewCaseList = lists:map(fun(OldCase) -> OldRest ++ OldCase end, OldCaseList),
-                        {CommonPart, [NewRest] ++ NewCaseList}
+                        CommonPart = calculate_common_part(OldCommonPart, BodyRest),
+                        {CommonPart, [BodySource] ++ OldCaseList}
                 end,
     dict:update(Key, UpdateFun, InitialValue, Storage).
 
--spec calculate_common_part(Str1 :: string(), Str2 :: string()) -> {CommonPart :: string, Rest1 :: string(), Rest2 :: string()}.
+-spec post_process_commands(Storage :: dict()) -> dict().
+post_process_commands(Storage) ->
+    MapFun = fun
+                (_Key, {CommonPart, [_SingleCase]}) -> {CommonPart, []};
+                (_Key, Value) -> Value
+             end,
+    Predicate = fun
+                   (_Key, {"", []}) -> false;
+                   (_Key, _Value) -> true
+                end,
+    dict:filter(Predicate, dict:map(MapFun, Storage)).
+
+-spec calculate_common_part(Str1 :: string(), Str2 :: string()) -> string().
 calculate_common_part(Str1, Str2) -> calculate_common_part(Str1, Str2, "").
 
--spec calculate_common_part(Rest1 :: string(), Rest2 :: string(), CommonPart :: string()) ->
-          {CommonPart :: string, Rest1 :: string(), Rest2 :: string()}.
-calculate_common_part("", "", CommonPart) ->
-    {lists:reverse(CommonPart), "", ""};
-calculate_common_part(Rest1, "", CommonPart) ->
-    {lists:reverse(CommonPart), Rest1, ""};
-calculate_common_part("", Rest2, CommonPart) ->
-    {lists:reverse(CommonPart), "", Rest2};
+-spec calculate_common_part(Rest1 :: string(), Rest2 :: string(), CommonPart :: string()) -> string().
+calculate_common_part("", "", CommonPart) -> lists:reverse(CommonPart);
+calculate_common_part(_Rest1, "", CommonPart) -> lists:reverse(CommonPart);
+calculate_common_part("", _Rest2, CommonPart) -> lists:reverse(CommonPart);
 calculate_common_part([Char | Rest1], [Char | Rest2], CommonPart) ->
     calculate_common_part(Rest1, Rest2, [Char] ++ CommonPart);
-calculate_common_part(Rest1, Rest2, CommonPart) ->
-    {lists:reverse(CommonPart), Rest1, Rest2}.
+calculate_common_part(_Rest1, _Rest2, CommonPart) -> lists:reverse(CommonPart).
 
 -spec join_command_body(CommandBodyParts :: [string()]) -> string().
 join_command_body(CommandBodyParts) ->
