f978e43ca1c16b8c9043d397d7a3b5672466c156
Ushakov Andrey <std_string@mail.ru>
2014-05-14 16:37:52 +0600

    start testing

---
+++ cli_common_tests/src/string_data_utils_tests.erl
@@ -0,0 +1,21 @@
+%% @author stdstring
+
+-module(string_data_utils_tests).
+
+-include_lib("eunit/include/eunit.hrl").
+
+%%-spec add_trailing_line_feed(Source :: string()) -> string().
+%%add_trailing_line_feed(Source) ->
+%%    remove_trailing_line_feed(Source) ++ "\n".
+
+%%-spec remove_trailing_line_feed(Source :: string()) -> string().
+%%remove_trailing_line_feed(Source) ->
+%%    string:strip(Source, right, $\n).
+
+%% ====================================================================
+%% Test functions
+%% ====================================================================
+
+add_trailing_line_feed_test_() -> [].
+
+remove_trailing_line_feed_test_() -> [].
