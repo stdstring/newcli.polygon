09c245b3a40883e79acc5f6d6a921ff32898bccb
Ushakov Andrey <std_string@mail.ru>
2014-05-18 17:46:28 +0600

    tests on crypto_utils module

---
+++ cli_common_tests/src/crypto_utils_tests.erl
@@ -0,0 +1,20 @@
+%% @author stdstring
+
+-module(crypto_utils_tests).
+
+-include_lib("eunit/include/eunit.hrl").
+
+-define(SOURCE_DATA, "iddqd idkfa").
+-define(SALT, "some salt value").
+
+%% ====================================================================
+%% Test functions
+%% ====================================================================
+
+hash_test_() ->
+    [{"sha512 hash", ?_assertEqual(expected_hash_impl(sha512, ?SOURCE_DATA), crypto_utils:hash(sha512, ?SOURCE_DATA, ?SALT))},
+     {"sha256 hash", ?_assertEqual(expected_hash_impl(sha256, ?SOURCE_DATA), crypto_utils:hash(sha256, ?SOURCE_DATA, ?SALT))}].
+
+expected_hash_impl(Type, Data) ->
+    IntermediateDigest = crypto:hash(Type, Data),
+    crypto:hash(Type, list_to_binary([IntermediateDigest, ?SALT])).
