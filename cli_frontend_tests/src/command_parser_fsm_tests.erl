e0d0482401109a9656fbb340b3cd409b8e172d55
Ushakov Andrey <std_string@mail.ru>
2014-05-22 16:57:15 +0600

    project for tests on cli_frontend

---
+++ cli_frontend_tests/src/command_parser_fsm_tests.erl
@@ -0,0 +1,12 @@
+%% @author stdstring
+
+-module(command_parser_fsm_tests).
+
+-include_lib("eunit/include/eunit.hrl").
+
+%% ====================================================================
+%% Test functions
+%% ====================================================================
+
+process_token_test_() ->
+    [].
