6190e3e14bc02a030b02ff1e1c021b44c90a8557
Ushakov Andrey <std_string@mail.ru>
2014-05-23 17:37:05 +0600

    preparation for integration tests

---
+++ cli_integration_tests/src/integration_tests.erl
@@ -0,0 +1,12 @@
+%% @author stdstring
+
+-module(string_data_utils_tests).
+
+-include_lib("eunit/include/eunit.hrl").
+
+%% ====================================================================
+%% Test functions
+%% ====================================================================
+ 
+ integration_test_() ->
+    [].
