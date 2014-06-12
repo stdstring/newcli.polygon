0bb81cf96ae0bd8e01e948dc68b5c80697f1051b
Ushakov Andrey <std_string@mail.ru>
2014-06-12 16:53:31 +0600

    login and logout integration tests

--- cli_integration_tests/src/login_integration_tests.erl
+++ cli_integration_tests/src/login_integration_tests.erl
@@ -8,5 +8,7 @@
 
 integration_test_() ->
     integration_tests_common:create_tests_entry([
-    	{["login", "guest", "idclip", "logout"], [?LOGIN, "some greeting message", "guest@CliDemo>You are logged out."], "login: simple login and logout"},
-    	{["login", "guest", "666"], [?LOGIN, "Login's attempt is failed due to the following: {authentication_fail,bad_password}", "Command execution failed. Return code is 255"], "login: bad password"}]).
+    	{["login", "guest", "idclip", "logout"], [?LOGIN, "some greeting message", "guest@CliDemo>You are logged out."], "login: user login and logout"},
+    	{["login", "root", "iddqd", "logout"], [?LOGIN, "some greeting message", "root@CliDemo#You are logged out."], "login: admin login and logout"},
+    	{["login", "guest", "666"], [?LOGIN, "Login's attempt is failed due to the following: {authentication_fail,bad_password}", "Command execution failed. Return code is 255"], "login: bad password"},
+    	{["login", "other_user", "666"], [?LOGIN, "Login's attempt is failed due to the following: {authentication_fail,unknown_username}", "Command execution failed. Return code is 255"], "login: unknown login"}]).
