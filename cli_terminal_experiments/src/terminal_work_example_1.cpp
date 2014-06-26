d3332190e6c31c2693f3accb0097b04d682ba000
Ushakov Andrey <std_string@mail.ru>
2014-06-26 18:37:26 +0600

    improvements into example

--- cli_terminal_experiments/src/terminal_work_example_1.cpp
+++ cli_terminal_experiments/src/terminal_work_example_1.cpp
@@ -2,29 +2,30 @@
 #include <unistd.h>
 #include <termios.h>
 
-#define FD_INPUT 0
-
 void check_terminal();
 termios get_term_attrs();
 void set_term_attrs(termios const &attrs);
 termios construct_noncanon_mode(termios const &source_attrs);
 void check_noncanon_mode();
+void main_loop();
 
 int main()
 {
-    std::cout << "cli_terminal_example" << std::endl;
+    std::cout << "cli_terminal_example start" << std::endl;
     check_terminal();
     termios old_attrs = get_term_attrs();
     termios new_attrs = construct_noncanon_mode(old_attrs);
     set_term_attrs(new_attrs);
     check_noncanon_mode();
+    main_loop();
     set_term_attrs(old_attrs);
+    std::cout << "cli_terminal_example finish" << std::endl;
     return 0;
 }
 
 void check_terminal()
 {
-    if (!isatty(FD_INPUT))
+    if (!isatty(STDIN_FILENO))
         exit(-1);
     std::cout << "0 is terminal" << std::endl;
 }
@@ -32,21 +33,22 @@ void check_terminal()
 termios get_term_attrs()
 {
     termios buf;
-    if (tcgetattr(FD_INPUT, &buf) < 0)
+    if (tcgetattr(STDIN_FILENO, &buf) < 0)
         exit(-1);
     return buf;
 }
 
 void set_term_attrs(termios const &attrs)
 {
-    if (tcsetattr(FD_INPUT, TCSANOW, &attrs) < 0)
+    if (tcsetattr(STDIN_FILENO, TCSANOW, &attrs) < 0)
         exit(-1);
 }
 
 termios construct_noncanon_mode(termios const &source_attrs)
 {
     termios dest_attrs(source_attrs);
-    dest_attrs.c_lflag &= ~(ECHO | ICANON);
+    dest_attrs.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+    dest_attrs.c_iflag &= ~(BRKINT | ICRNL | ISTRIP | IXON);
     dest_attrs.c_cc[VMIN] = 1;
     dest_attrs.c_cc[VTIME] = 0;
     return dest_attrs;
@@ -55,6 +57,21 @@ termios construct_noncanon_mode(termios const &source_attrs)
 void check_noncanon_mode()
 {
     termios attrs = get_term_attrs();
-    if ((attrs.c_lflag & (ECHO | ICANON)) || attrs.c_cc[VMIN] != 1 || attrs.c_cc[VTIME] != 0)
+    if ((attrs.c_lflag & (ECHO | ICANON | IEXTEN | ISIG)) ||
+        (attrs.c_iflag & (BRKINT | ICRNL | ISTRIP | IXON)) || 
+        attrs.c_cc[VMIN] != 1 ||
+        attrs.c_cc[VTIME] != 0)
         exit(-1);
+}
+
+void main_loop()
+{
+    int buf;
+    while(read(STDIN_FILENO, &buf, 1) == 1)
+    {
+        std::cout << "Pressed: " << buf << std::endl;
+        std::cout << "Char: " << (char) buf << std::endl;
+        if ((buf &= 255) == 0177)
+            break;
+    }
 }
