e516e39831425d9f04bbcbf9af63ceba918cc4ab
Ushakov Andrey <std_string@mail.ru>
2014-07-09 15:13:11 +0600

    continue prototype of terminal

--- cli_terminal_experiments/src/terminal_work_example_server.erl
+++ cli_terminal_experiments/src/terminal_work_example_server.erl
@@ -1,3 +1,22 @@
 -module(terminal_work_example_server).
 
 -export([]).
+
+-record(server_state, {socket = none, timeout = infinity, current_comand = none}).
+
+start_server(Port, Timeout) ->
+    {ok, ListenSocket} = gen_tcp:listen(Port, [binary, {packet, 4}, {active, once}]),
+    {ok, Socket} = gen_tcp:accept(ListenSocket),
+    server_event_loop(Socket, Timeout).
+
+server_event_loop(Socket, Timeout) ->
+    receive
+        {tcp, Socket, Data} ->
+            process_data(Data, Socket),
+            inet:setopts(Socket, [{active, once}]),
+            server_event_loop(Socket, Timeout);
+        {tcp_closed, Socket} -> ok
+    after Timeout -> ok
+    end.
+
+process_data(Data, Socket) -> ok.
