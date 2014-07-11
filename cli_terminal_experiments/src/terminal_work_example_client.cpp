aa4921d6fed5bdc89e79113c57bf780407fc5220
Ushakov Andrey <std_string@mail.ru>
2014-07-11 17:29:32 +0600

    continue create terminal client

--- cli_terminal_experiments/src/terminal_work_example_client.cpp
+++ cli_terminal_experiments/src/terminal_work_example_client.cpp
@@ -1,6 +1,7 @@
 // C++
 #include <cstdlib>
 #include <iostream>
+#include <utility>
 // C
 #include <strings.h>
 // network
@@ -11,33 +12,81 @@
 #define IP_ADDRESS "127.0.0.1"
 #define PORT 22222
 
+// declaration
+int create_socket();
+void connect(int socketd);
+std::pair<std::string, std::string> read_message(int socketd);
+void write_message(int socketd, std::string const &message);
+
+// global variables
+bool running = true;
+
 int main()
 {
     std::cout << "start terminal_work_example_client" << std::endl;
+    int socketd = create_socket();
+    connect(socketd);
+    while (running)
+    {
+        
+    }
     std::cout << "finish terminal_work_example_client" << std::endl;
     return 0;
 }
 
 int create_socket()
 {
-    struct sockaddr_in addr;
-    bzero(&addr, sizeof(addr));
-    addr.sin_family = AF_INET;
-    addr.sin_port = htons(PORT);
-    int ip_binary;
-    if (inet_pton(AF_INET, IP_ADDRESS, &ip_binary) != 1)
-    {
-        std::exit(-1);
-    }
-    addr.sin_addr.s_addr = ip_binary;
     int socketd = socket(AF_INET, SOCK_STREAM, 0);
     if (socketd == -1)
     {
-        std::exit(-1);
-    }
-    if (bind(socketd, (struct sockaddr*) (&addr), sizeof(addr)) == -1)
-    {
+        std::cout << "error in socket" << std::endl;
         std::exit(-1);
     }
     return socketd;
 }
+
+void connect(int socketd)
+{
+    struct sockaddr_in server_addr;
+    bzero(&server_addr, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(PORT);
+    int ip_binary;
+    if (inet_pton(AF_INET, IP_ADDRESS, &ip_binary) != 1)
+    {
+        std::cout << "error in inet_pton" << std::endl;
+        std::exit(-1);
+    }
+    server_addr.sin_addr.s_addr = ip_binary;
+    if (connect(socketd, reinterpret_cast<sockaddr*>(&server_addr), sizeof(server_addr)) == -1)
+    {
+        std::cout << "error in connect" << std::endl;
+        std::exit(-1);
+    }
+}
+
+std::pair<std::string, std::string> read_message(int socketd)
+{
+    // read 4-bytes length
+    int length_binary;
+    if (recv(socketd, &length_binary, sizeof(int), MSG_WAITALL) != sizeof(int))
+    {
+        std::cout << "error in read 4-bytes length" << std::endl;
+        std::exit(-1);
+    }
+    int length = ntohl(length_binary);
+    // read body
+    char *buffer = new char[length];
+    if (recv(socketd, buffer, length, MSG_WAITALL) != length)
+    {
+        std::cout << "error in read body" << std::endl;
+        std::exit(-1);
+    }
+    delete[] buffer;
+    // data deserialization
+    // ...
+    return std::pair<std::string, std::string>("", "");
+}
+
+void write_message(int socketd, std::string const &message)
+{}
