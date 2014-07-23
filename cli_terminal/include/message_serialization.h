ffdc44dcfd0ace51f28ff8e44572a8a693396982
Ushakov Andrey <std_string@mail.ru>
2014-07-23 16:35:14 +0600

    start message serialization

--- cli_terminal/include/message_serialization.h
+++ cli_terminal/include/message_serialization.h
@@ -1,3 +1,12 @@
 #ifndef H_MESSAGE_SERIALIZATION
 #define H_MESSAGE_SERIALIZATION
+
+#include <memory>
+#include "cterm_ptr.h"
+#include "message.h"
+
+typedef std::unique_ptr<unsigned char[]> byte_array_ptr;
+
+ byte_array_ptr serialize(CommandRequest const &request);
+
 #endif
