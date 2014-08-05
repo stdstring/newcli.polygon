1dce4b1892984a3df4d2c6f97334e3a5eb903c15
Ushakov Andrey <std_string@mail.ru>
2014-08-05 18:51:32 +0600

    continue create infrastructure for interaction with cli_terminal

--- cli_frontend/src/cli_terminal_listen_endpoint.erl
+++ cli_frontend/src/cli_terminal_listen_endpoint.erl
@@ -1,3 +1,28 @@
 %% @author std-string
 
 -module(cli_terminal_listen_endpoint).
+
+-behaviour(gen_server).
+
+%% ====================================================================
+%% API functions
+%% ====================================================================
+
+%% gen_server export
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
+
+init(_State) -> ok.
+
+handle_call(_Request, _From, State) -> {stop, enotsup, State}.
+
+handle_cast(_Request, State) -> {stop, enotsup, State}.
+
+handle_info(_Info, State) -> {stop, enotsup, State}.
+
+terminate(_Reason, _State) -> ok.
+
+code_change(_OldVsn, State, _Extra) -> {ok, State}.
+
+%% ====================================================================
+%% Internal functions
+%% ====================================================================
