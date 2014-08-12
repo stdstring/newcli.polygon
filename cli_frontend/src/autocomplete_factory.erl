1321cfad15ee9231a704feb54c7b9ac53aa29b89
Ushakov Andrey <std_string@mail.ru>
2014-08-12 17:05:51 +0600

    start correct autocomplete_factory

--- cli_frontend/src/autocomplete_factory.erl
+++ cli_frontend/src/autocomplete_factory.erl
@@ -2,29 +2,30 @@
 
 -module(autocomplete_factory).
 
--include("logic_utils_defs.hrl").
--include("common_defs.hrl").
-
 %% ====================================================================
 %% API functions
 %% ====================================================================
 
--export([create_expand_fun/1]).
+-export([]).
+%% temporary export
+-export([process_command/2, process_command/4, add_case/3, join_command_body/1, extra_spaces_cleanup/1, extra_spaces_cleanup/3]).
 
--spec create_expand_fun(ExecutionState :: #execution_state{}) -> fun((string()) -> {'yes' | 'no', string(), [string()]}).
-create_expand_fun(ExecutionState) ->
-    CommandsInfo = ExecutionState#execution_state.commands_info,
-    AccFun = fun({_Name, Body, _Help}, Storage) -> process_command(join_command_body(Body), Storage) end,
-    ResultStorage = post_process_commands(lists:foldl(AccFun, dict:new(), CommandsInfo)),
-    fun(Input) ->
-            PreparedInput = extra_spaces_cleanup(Input),
-            case dict:is_key(PreparedInput, ResultStorage) of
-                true ->
-                    {ok, {CommonPart, CaseList}} = dict:find(PreparedInput, ResultStorage),
-                    {yes, CommonPart, CaseList};
-                false -> {yes, "", []}
-            end
-    end.
+%%-export([create_expand_fun/1]).
+
+%%-spec create_expand_fun(ExecutionState :: #execution_state{}) -> fun((string()) -> {'yes' | 'no', string(), [string()]}).
+%%create_expand_fun(ExecutionState) ->
+%%    CommandsInfo = ExecutionState#execution_state.commands_info,
+%%    AccFun = fun({_Name, Body, _Help}, Storage) -> process_command(join_command_body(Body), Storage) end,
+%%    ResultStorage = post_process_commands(lists:foldl(AccFun, dict:new(), CommandsInfo)),
+%%    fun(Input) ->
+%%            PreparedInput = extra_spaces_cleanup(Input),
+%%            case dict:is_key(PreparedInput, ResultStorage) of
+%%                true ->
+%%                    {ok, {CommonPart, CaseList}} = dict:find(PreparedInput, ResultStorage),
+%%                    {yes, CommonPart, CaseList};
+%%                false -> {yes, "", []}
+%%            end
+%%    end.
 
 %% ====================================================================
 %% Internal functions
@@ -38,44 +39,18 @@ process_command(Body, Storage) ->
 process_command(_BodySource, "", _Key, Storage) -> Storage;
 process_command(BodySource, BodyRest, undefined, Storage) ->
     NewKey = "",
-    NewStorage = process_body(BodySource, BodyRest, NewKey, Storage),
+    NewStorage = add_case(BodySource, NewKey, Storage),
     process_command(BodySource, BodyRest, NewKey, NewStorage);
 process_command(BodySource, [Header | BodyRest], Key, Storage) ->
-    NewKey = [Header] ++ Key,
-    NewStorage = process_body(BodySource, BodyRest, NewKey, Storage),
+    NewKey = Key ++ [Header],
+    NewStorage = add_case(BodySource, NewKey, Storage),
     process_command(BodySource, BodyRest, NewKey, NewStorage).
 
--spec process_body(BodySource :: string(), BodyRest :: string(), Key :: string(), Storage :: dict()) -> dict().
-process_body(BodySource, BodyRest, Key, Storage) ->
-    InitialValue = {BodyRest, [BodySource]},
-    UpdateFun = fun({OldCommonPart, OldCaseList}) ->
-                        CommonPart = calculate_common_part(OldCommonPart, BodyRest),
-                        {CommonPart, [BodySource] ++ OldCaseList}
-                end,
+-spec add_case(BodySource :: string(), Key :: string(), Storage :: dict()) -> dict().
+add_case(BodySource, Key, Storage) ->
+    InitialValue = [BodySource],
+    UpdateFun = fun(OldCaseList) -> [BodySource] ++ OldCaseList end,
     dict:update(Key, UpdateFun, InitialValue, Storage).
-
--spec post_process_commands(Storage :: dict()) -> dict().
-post_process_commands(Storage) ->
-    MapFun = fun
-                (_Key, {CommonPart, [_SingleCase]}) -> {CommonPart, []};
-                (_Key, Value) -> Value
-             end,
-    Predicate = fun
-                   (_Key, {"", []}) -> false;
-                   (_Key, _Value) -> true
-                end,
-    dict:filter(Predicate, dict:map(MapFun, Storage)).
-
--spec calculate_common_part(Str1 :: string(), Str2 :: string()) -> string().
-calculate_common_part(Str1, Str2) -> calculate_common_part(Str1, Str2, "").
-
--spec calculate_common_part(Rest1 :: string(), Rest2 :: string(), CommonPart :: string()) -> string().
-calculate_common_part("", "", CommonPart) -> lists:reverse(CommonPart);
-calculate_common_part(_Rest1, "", CommonPart) -> lists:reverse(CommonPart);
-calculate_common_part("", _Rest2, CommonPart) -> lists:reverse(CommonPart);
-calculate_common_part([Char | Rest1], [Char | Rest2], CommonPart) ->
-    calculate_common_part(Rest1, Rest2, [Char] ++ CommonPart);
-calculate_common_part(_Rest1, _Rest2, CommonPart) -> lists:reverse(CommonPart).
 
 -spec join_command_body(CommandBodyParts :: [string()]) -> string().
 join_command_body(CommandBodyParts) ->
