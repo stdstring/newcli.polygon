38eb48b63e4f39719cc73ba9ccc14078504374dd
Ushakov Andrey <std_string@mail.ru>
2014-08-14 23:37:19 +0600

    continue create client handler

--- cli_frontend/src/client_handler.erl
+++ cli_frontend/src/client_handler.erl
@@ -4,28 +4,47 @@
 
 -include("common_defs.hrl").
 
--record(client_handler_state, {config :: #global_config{}, state :: #execution_state{}}).
+-record(client_handler_state, {config :: #global_config{},
+                               state :: #execution_state{},
+                               endpoint = undefined :: 'undefined' | pid(),
+                               extension_generator = undefined :: 'undefined' | fun((string()) -> [string()])}).
 
 %% ====================================================================
 %% API functions
 %% ====================================================================
 
--export([start/1, get_current_state/1, get_extensions/2]).
+-export([start/1, process_command/2, interrupt_command/1, get_current_state/1, get_extensions/2, exit/1]).
 %% gen_server export
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
 
 start(Socket) ->
     gen_server:start_link(?MODULE, Socket, []).
 
-get_current_state(Handler) -> ok.
+process_command(Handler, CommandLine) ->
+    gen_server:cast(Handler, {process_command, CommandLine}).
 
-get_extensions(Handler, CommandLine) -> ok.
+interrupt_command(Handler) ->
+    gen_server:cast(Handler, interrupt_command).
+
+get_current_state(Handler) ->
+    gen_server:call(Handler, current_state).
+
+get_extensions(Handler, CommandLine) ->
+    gen_server:call(Handler, {extensions, CommandLine}).
+
+%%exit(Handler) -> gen_server:cast(Handler, exit).
+exit(_Handler) -> ok.
 
 init(_Args) -> {ok, #client_handler_state{}}.
 
-handle_call(_Request, _From, State) -> {stop, enotsup, State}.
+handle_call(current_state, _From, State) -> {stop, enotsup, State};
+handle_call({extensions, CommandLine}, _From, State) ->
+    ExtensionGenerator = State#client_handler_state.extension_generator,
+    Extensions = ExtensionGenerator(CommandLine),
+    {reply, Extensions, State}.
 
-handle_cast(_Request, State) -> {stop, enotsup, State}.
+handle_cast({process_command, _CommandLine}, State) -> {stop, enotsup, State};
+handle_cast(interrupt_command, State) -> {stop, enotsup, State}.
 
 handle_info(_Info, State) -> {stop, enotsup, State}.
 
