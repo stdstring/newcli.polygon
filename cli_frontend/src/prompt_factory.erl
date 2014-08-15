310394d9779791f08a59c06437ba2e996f3f15f9
Ushakov Andrey <std_string@mail.ru>
2014-08-15 13:41:00 +0600

    refactor prompt_factory

--- cli_frontend/src/prompt_factory.erl
+++ cli_frontend/src/prompt_factory.erl
@@ -3,6 +3,10 @@
 -include("common_defs.hrl").
 -include("logic_utils_defs.hrl").
 
+-define(ADMIN_FOOTER, "#").
+-define(USER_FOOTER, ">").
+-define(PROMPT_FORMAT, "~s@~s~s~s").
+
 -export([generate_prompt/1]).
 
 -spec generate_prompt(ExecutionState :: #execution_state{}) -> string().
@@ -13,6 +17,6 @@ generate_prompt(ExecutionState) ->
     CliMode = ExecutionState#execution_state.current_cli_mode,
     CliModePart = logic_utils:ternary_lazy_op((CliMode == undefined) or (CliMode == ""), ?LAZY(""), ?LAZY(" (" ++ CliMode ++ ")")),
     IsAdmin = logic_utils:ternary_lazy_op(LoginInfo == undefined, ?LAZY(false), ?LAZY(LoginInfo#login_info.is_admin)),
-    PromptFooter = logic_utils:ternary_op(IsAdmin, "#", ">"),
-    Prompt = io_lib:format("~s@~s~s~s", [LoginPart, DeviceName, CliModePart, PromptFooter]),
+    PromptFooter = logic_utils:ternary_op(IsAdmin, ?ADMIN_FOOTER, ?USER_FOOTER),
+    Prompt = io_lib:format(?PROMPT_FORMAT, [LoginPart, DeviceName, CliModePart, PromptFooter]),
     lists:flatten(Prompt).
