ba5bc850c392bc1b4abc4f373a2abf7ef45c552a
Ushakov Andrey <std_string@mail.ru>
2014-09-11 11:53:30 +0600

    templates fot login command behavior

---
+++ cli_terminal/include/login_command_terminal_behavior.h
@@ -0,0 +1,19 @@
+#ifndef H_LOGIN_COMMAND_TERMINAL_BEHAVIOR
+#define H_LOGIN_COMMAND_TERMINAL_BEHAVIOR
+
+#include "iterminal_behavior.h"
+
+namespace cli_terminal
+{
+
+class login_command_terminal_behavior : public iterminal_behavior
+{
+public:
+    void install_input_action() override;
+    void install_signal_action() override;
+    void process_char() override;
+};
+
+}
+
+#endif
---
+++ cli_terminal/src/login_command_terminal_behavior.cpp
@@ -0,0 +1,21 @@
+#include "login_command_terminal_behavior.h"
+
+namespace cli_terminal
+{
+
+void login_command_terminal_behavior::install_input_action()
+{
+    // do nothing now
+}
+
+void login_command_terminal_behavior::install_signal_action()
+{
+    // do nothing now
+}
+
+void login_command_terminal_behavior::process_char()
+{
+    // do nothing now
+}
+
+}
