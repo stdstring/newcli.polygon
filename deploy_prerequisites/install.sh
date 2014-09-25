145bc7943bf572c325e412141d7247a1b41a2840
Ushakov Andrey <std_string@mail.ru>
2014-09-25 22:28:53 +0600

    yet one simple changes into install.sh

--- deploy_prerequisites/install.sh
+++ deploy_prerequisites/install.sh
@@ -3,24 +3,24 @@
 rm -rf /etc/backend
 mkdir /etc/backend
 cp -t /etc/backend cli_backend_data/*
-chmod 444 /etc/backend/*
+chmod 0444 /etc/backend/*
 # /etc/frontend
 rm -rf /etc/frontend
 mkdir /etc/frontend
 cp -t /etc/frontend cli_frontend_data/*
-chmod 444 /etc/frontend/*
+chmod 0444 /etc/frontend/*
 # backend libs
 rm -rf /usr/local/lib/backend
 mkdir /usr/local/lib/backend
 cp -t /usr/local/lib/backend cli_backend_ebin/*
 rm /usr/local/lib/backend/cli_backend_start.sh
-chmod 444 /usr/local/lib/backend/*
+chmod 0444 /usr/local/lib/backend/*
 # frontend libs
 rm -rf /usr/local/lib/frontend
 mkdir /usr/local/lib/frontend
 cp -t /usr/local/lib/frontend cli_frontend_ebin/*
 rm /usr/local/lib/frontend/cli_frontend_start.sh
-chmod 444 /usr/local/lib/frontend/*
+chmod 0444 /usr/local/lib/frontend/*
 # binaries
 rm /usr/local/bin/cli_backend_start.sh
 rm /usr/local/bin/cli_frontend_start.sh
@@ -28,11 +28,11 @@ rm /usr/local/bin/cli_terminal
 cp cli_backend_ebin/cli_backend_start.sh /usr/local/bin
 cp cli_frontend_ebin/cli_frontend_start.sh /usr/local/bin
 cp cli_terminal_bin/cli_terminal /usr/local/bin
-chmod 755 /usr/local/bin/cli_backend_start.sh
-chmod 755 /usr/local/bin/cli_frontend_start.sh
-chmod 555 /usr/local/bin/cli_terminal
+chmod 0755 /usr/local/bin/cli_backend_start.sh
+chmod 0755 /usr/local/bin/cli_frontend_start.sh
+chmod 0555 /usr/local/bin/cli_terminal
 # cli_user
 userdel cli_user
 useradd -s /usr/local/bin/cli_terminal cli_user
 # set passwd, e.g. password=1
-# passwd cli_user #
+# passwd cli_user
