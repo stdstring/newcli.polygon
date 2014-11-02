d00fd3f72f2cf4ceffbf49ee3b489df265a7f121
Ushakov Andrey <std_string@mail.ru>
2014-11-02 09:30:54 +0500

    add tests on module regeneration

--- cli_command_parser_prototype/src/dynamic_module_tests.erl
+++ cli_command_parser_prototype/src/dynamic_module_tests.erl
@@ -9,31 +9,43 @@
 -define(POSITIVE, "positive").
 -define(ZERO, "zero").
 -define(NEGATIVE, "negative").
+-define(POSITIVE_OTHER, "other positive").
+-define(ZERO_OTHER, "other zero").
+-define(NEGATIVE_OTHER, "other negative").
 
 %% ====================================================================
 %% Test functions
 %% ====================================================================
 
 generate_erl_syntax_module_test_() ->
-    ModuleBinary = generate_erl_syntax_module(),
+    ModuleBinary = generate_erl_syntax_module(?POSITIVE, ?ZERO, ?NEGATIVE),
     {module, ?MODULE_NAME} = code:load_binary(?MODULE_NAME, [], ModuleBinary),
     [{"positive case", ?_assertEqual(?POSITIVE, ?MODULE_NAME:?FUNCTION_NAME(666))},
      {"zero case", ?_assertEqual(?ZERO, ?MODULE_NAME:?FUNCTION_NAME(0))},
      {"negative case", ?_assertEqual(?NEGATIVE, ?MODULE_NAME:?FUNCTION_NAME(-13))}].
 
 generate_abstract_format_module_test_() ->
-    ModuleBinary = generate_abstract_format_module(),
+    ModuleBinary = generate_abstract_format_module(?POSITIVE, ?ZERO, ?NEGATIVE),
     {module, ?MODULE_NAME} = code:load_binary(?MODULE_NAME, [], ModuleBinary),
     [{"positive case", ?_assertEqual(?POSITIVE, ?MODULE_NAME:?FUNCTION_NAME(666))},
      {"zero case", ?_assertEqual(?ZERO, ?MODULE_NAME:?FUNCTION_NAME(0))},
      {"negative case", ?_assertEqual(?NEGATIVE, ?MODULE_NAME:?FUNCTION_NAME(-13))}].
 
+regenerate_module_test_() ->
+    OldModuleBinary = generate_abstract_format_module(?POSITIVE, ?ZERO, ?NEGATIVE),
+    {module, ?MODULE_NAME} = code:load_binary(?MODULE_NAME, [], OldModuleBinary),
+    NewModuleBinary = generate_abstract_format_module(?POSITIVE_OTHER, ?ZERO_OTHER, ?NEGATIVE_OTHER),
+    {module, ?MODULE_NAME} = code:load_binary(?MODULE_NAME, [], NewModuleBinary),
+    [{"positive case", ?_assertEqual(?POSITIVE_OTHER, ?MODULE_NAME:?FUNCTION_NAME(666))},
+     {"zero case", ?_assertEqual(?ZERO_OTHER, ?MODULE_NAME:?FUNCTION_NAME(0))},
+     {"negative case", ?_assertEqual(?NEGATIVE_OTHER, ?MODULE_NAME:?FUNCTION_NAME(-13))}].
+
 %% ====================================================================
 %% Internal functions
 %% ====================================================================
 
--spec generate_erl_syntax_module() -> binary().
-generate_erl_syntax_module() ->
+-spec generate_erl_syntax_module(PositiveValue :: string(), ZeroValue :: string(), NegativeValue :: string()) -> binary().
+generate_erl_syntax_module(PositiveValue, ZeroValue, NegativeValue) ->
     % fun(X) when X > 0 -> "positive";
     % fun(X) when X == 0 -> "zero";
     % fun(X) when X < 0 -> "negative".
@@ -44,35 +56,32 @@ generate_erl_syntax_module() ->
     ExportForm = erl_syntax:revert(ExportDef),
     FunArg = erl_syntax:variable("A"),
     PositiveGuard = [erl_syntax:infix_expr(FunArg, erl_syntax:operator(">"), erl_syntax:integer(0))],
-    PositiveBody = [erl_syntax:string(?POSITIVE)],
+    PositiveBody = [erl_syntax:string(PositiveValue)],
     PositiveClause = erl_syntax:clause([FunArg], PositiveGuard, PositiveBody),
     ZeroGuard = [erl_syntax:infix_expr(FunArg, erl_syntax:operator("=="), erl_syntax:integer(0))],
-    ZeroBody = [erl_syntax:string(?ZERO)],
+    ZeroBody = [erl_syntax:string(ZeroValue)],
     ZeroClause = erl_syntax:clause([FunArg], ZeroGuard, ZeroBody),
     NegativeGuard = [erl_syntax:infix_expr(FunArg, erl_syntax:operator("<"), erl_syntax:integer(0))],
-    NegativeBody = [erl_syntax:string(?NEGATIVE)],
+    NegativeBody = [erl_syntax:string(NegativeValue)],
     NegativeClause = erl_syntax:clause([FunArg], NegativeGuard, NegativeBody),
     Fun = erl_syntax:function(erl_syntax:atom(?FUNCTION_NAME), [PositiveClause, ZeroClause, NegativeClause]),
     FunForm = erl_syntax:revert(Fun),
-    % io:format(user, "ModuleForm: ~p~n", [ModuleForm]),
-    % io:format(user, "ExportForm: ~p~n", [ExportForm]),
-    % io:format(user, "FunForm: ~p~n", [FunForm]),
     {ok, ?MODULE_NAME, ModuleBinary} = compile:forms([ModuleForm, ExportForm, FunForm]),
     ModuleBinary.
 
--spec generate_abstract_format_module() -> binary().
-generate_abstract_format_module() ->
+-spec generate_abstract_format_module(PositiveValue :: string(), ZeroValue :: string(), NegativeValue :: string()) -> binary().
+generate_abstract_format_module(PositiveValue, ZeroValue, NegativeValue) ->
     % fun(X) when X > 0 -> "positive";
     % fun(X) when X == 0 -> "zero";
     % fun(X) when X < 0 -> "negative".
     ModuleForm = {attribute, 0, module, ?MODULE_NAME},
     ExportForm = {attribute, 0, export, [{?FUNCTION_NAME, 1}]},
     PositiveGuard = {op, 0, '>', {var, 0, 'A'}, {integer, 0, 0}},
-    PositiveClause = {clause, 0, [{var, 0, 'A'}], [[PositiveGuard]], [{string, 0, ?POSITIVE}]},
+    PositiveClause = {clause, 0, [{var, 0, 'A'}], [[PositiveGuard]], [{string, 0, PositiveValue}]},
     ZeroGuard = {op, 0, '==', {var, 0, 'A'}, {integer, 0, 0}},
-    ZeroClause = {clause, 0, [{var, 0, 'A'}], [[ZeroGuard]], [{string, 0, ?ZERO}]},
+    ZeroClause = {clause, 0, [{var, 0, 'A'}], [[ZeroGuard]], [{string, 0, ZeroValue}]},
     NegativeGuard = {op, 0, '<', {var, 0, 'A'}, {integer, 0, 0}},
-    NegativeClause = {clause, 0, [{var, 0, 'A'}], [[NegativeGuard]], [{string, 0, ?NEGATIVE}]},
+    NegativeClause = {clause, 0, [{var, 0, 'A'}], [[NegativeGuard]], [{string, 0, NegativeValue}]},
     FunForm = {function, 0, ?FUNCTION_NAME, 1, [PositiveClause, ZeroClause, NegativeClause]},
     {ok, ?MODULE_NAME, ModuleBinary} = compile:forms([ModuleForm, ExportForm, FunForm]),
     ModuleBinary.
