08e6f5f3553d822095025135a05a5bc57c661ebc
Ushakov Andrey <std_string@mail.ru>
2014-11-04 21:40:49 +0500

    syntax analyzer. start

---
+++ cli_command_parser_prototype/include/common_defs.hrl
@@ -0,0 +1,4 @@
+%% common definitions
+-record(token, {type = undefined, value = undefined}).
+-record(terminal, {type = value, value = undefined}).
+-record(nonterminal, {name = ""}).
--- cli_command_parser_prototype/src/syntax_analyzer.erl
+++ cli_command_parser_prototype/src/syntax_analyzer.erl
@@ -2,14 +2,55 @@
 
 -module(syntax_analyzer).
 
--export([process/3]).
+-export([process/2]).
+
+-include("common_defs.hrl").
+
+-record(process_state, {token_list = [], process_stack = []}).
 
 %% ====================================================================
 %% API functions
 %% ====================================================================
 
-process(_TokenList, _SyntaxTable, _CommandTable) -> ok.
+%%process(_TokenList, _SyntaxTable, _CommandTable) -> ok.
+
+process(_TokenList, _SyntaxTable) -> ok.
 
 %% ====================================================================
 %% Internal functions
 %% ====================================================================
+
+%% SyntaxTable = dict({nonterminal.Name, token.Type, token.Value | undefined} -> Production | error)
+process_token([Token | TokenListRest], [#terminal{type = Type, value = Value} | ProcessStackRest], SyntaxTable) ->
+    case check_terminal(Token, #terminal{type = Type, value = Value}) of
+        true ->
+            %% add processed token to some state
+            #process_state{token_list = TokenListRest, process_stack = ProcessStackRest};
+        false -> bad_token
+    end;
+process_token([Token | TokenListRest] = TokenList, [#nonterminal{name = Name} | ProcessStackRest], SyntaxTable) ->
+    case find_production(#nonterminal{name = Name}, Token, SyntaxTable) of
+        {ok, Production} ->
+            NewProcessStack = process_production(Production, ProcessStackRest),
+            #process_state{token_list = TokenListRest, process_stack = NewProcessStack};
+        not_found ->
+    end,
+    ok.
+
+check_terminal(#token{type = Type}, #terminal{type = Type, value = undefined}) -> true;
+check_terminal(#token{type = Type, value = Value}, #terminal{type = Type, value = Value}) -> true;
+check_terminal(#token{}, #terminal{}) -> false.
+
+find_production(Nonterminal, Token, SyntaxTable) ->
+    case dict:find({Nonterminal, Token}, SyntaxTable) of
+        {ok, Value} -> {ok, Value};
+        error ->
+            case dict:find({Nonterminal, Token#token{value = undefined}}, SyntaxTable) of
+                {ok, Value} -> {ok, Value};
+                error -> not_found
+            end
+    end.
+
+process_production(Production, ProcessStackRest) ->
+    %% run additional actions
+    Production ++ ProcessStackRest.
