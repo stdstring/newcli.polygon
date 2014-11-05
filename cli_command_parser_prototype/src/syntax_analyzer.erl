94f749502b3e799083a394bfec2d36299853bd94
Ushakov Andrey <std_string@mail.ru>
2014-11-05 17:58:12 +0600

    syntax analyzer. first approx. ready for testing

--- cli_command_parser_prototype/src/syntax_analyzer.erl
+++ cli_command_parser_prototype/src/syntax_analyzer.erl
@@ -12,30 +12,38 @@
 %% API functions
 %% ====================================================================
 
-%%process(_TokenList, _SyntaxTable, _CommandTable) -> ok.
-
-process(_TokenList, _SyntaxTable) -> ok.
+process(TokenList, SyntaxTable) ->
+    process(TokenList, [], SyntaxTable).
 
 %% ====================================================================
 %% Internal functions
 %% ====================================================================
 
+process([], [], _SyntaxTable) -> ok;
+process([], _ProcessStack, _SyntaxTable) -> bad_token;
+process(TokenList, ProcessStack, SyntaxTable) ->
+    case process_token(TokenList, ProcessStack, SyntaxTable) of
+        #process_state{token_list = NewTokenList, process_stack = NewProcessStack} -> process(NewTokenList, NewProcessStack, SyntaxTable);
+        bad_token -> bad_token
+    end.
+
 %% SyntaxTable = dict({nonterminal.Name, token.Type, token.Value | undefined} -> Production | error)
-process_token([Token | TokenListRest], [#terminal{type = Type, value = Value} | ProcessStackRest], SyntaxTable) ->
+process_token([Token | TokenListRest], [#terminal{type = Type, value = Value} | ProcessStackRest], _SyntaxTable) ->
     case check_terminal(Token, #terminal{type = Type, value = Value}) of
         true ->
             %% add processed token to some state
+            io:format("process terminal (type = ~p, value = ~p)~n", [Type, Value]),
             #process_state{token_list = TokenListRest, process_stack = ProcessStackRest};
         false -> bad_token
     end;
-process_token([Token | TokenListRest] = TokenList, [#nonterminal{name = Name} | ProcessStackRest], SyntaxTable) ->
+process_token([Token | TokenListRest], [#nonterminal{name = Name} | ProcessStackRest], SyntaxTable) ->
     case find_production(#nonterminal{name = Name}, Token, SyntaxTable) of
         {ok, Production} ->
+            io:format("process nonterminal (name = ~p)~n", [Name]),
             NewProcessStack = process_production(Production, ProcessStackRest),
             #process_state{token_list = TokenListRest, process_stack = NewProcessStack};
-        not_found ->
-    end,
-    ok.
+        not_found -> bad_token
+    end.
 
 check_terminal(#token{type = Type}, #terminal{type = Type, value = undefined}) -> true;
 check_terminal(#token{type = Type, value = Value}, #terminal{type = Type, value = Value}) -> true;
