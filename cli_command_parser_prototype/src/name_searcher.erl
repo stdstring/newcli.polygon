3151340ef83a491b324efcdaec6c004095f27d95
Ushakov Andrey <std_string@mail.ru>
2014-11-09 13:33:02 +0600

    finish with name searcher

--- cli_command_parser_prototype/src/name_searcher.erl
+++ cli_command_parser_prototype/src/name_searcher.erl
@@ -2,51 +2,64 @@
 
 -module(name_searcher).
 
--export([search_best/2, search/3]).
+-export([search_best/2, search/2]).
 
 %% ====================================================================
 %% API functions
 %% ====================================================================
 
-search_best(_Words, _SearchTable) -> ok.
+search_best(Words, Table) ->
+    search_best_impl([], Words, undefined, Table).
 
-%% SearchRow = [{Word :: string(), MinPrefixLength :: pos_integer()}]
-%% SearchTable = [{SearchRow, Module :: atom(), Function :: atom()}]
-search(_Words, _HasMore, []) -> {false, not_found};
-search(Words, true, SearchTable) ->
-    create_search_result(search_full_match(Words, SearchTable), search_incomplete_match(Words, SearchTable));
-search(Words, false, SearchTable) ->
-    create_search_result(search_full_match(Words, SearchTable), {false, not_found}).
+%% Condition = [{Word :: string(), MinPrefixLength :: pos_integer()}]
+%% Table = [{Condition, Module :: atom(), Function :: atom()}]
+search(_Words, []) -> false;
+search(Words, Table) ->
+    create_search_result(search_full_match(Words, Table), search_incomplete_match(Words, Table)).
 
 %% ====================================================================
 %% Internal functions
 %% ====================================================================
 
-create_search_result({false, not_found}, {false, not_found}) -> {false, not_found};
-create_search_result({true, Module, Func}, {false, not_found}) -> {true, Module, Func, []};
-create_search_result({false, not_found}, {incomplete, Rows}) -> {incomplete, Rows};
+search_best_impl(_WordsUsed, [], undefined, _Rows) -> false;
+search_best_impl(_WordsUsed, [], {Module, Func, Rest}, _Rows) -> {true, Module, Func, Rest};
+search_best_impl(_WordsUsed, _WordsRest, undefined, []) -> false;
+search_best_impl(_WordsUsed, _WordsRest, {Module, Func, Rest}, []) -> {true, Module, Func, Rest};
+search_best_impl(WordsUsed, [Word | Rest], Recognized, Rows) ->
+    NewWordsUsed = WordsUsed ++ [Word],
+    case search(NewWordsUsed, Rows) of
+        {true, Module, Func, RowsRest} ->
+            NewRecognized = {Module, Func, RowsRest},
+            search_best_impl(NewWordsUsed, Rest, NewRecognized, RowsRest);
+        {incomplete, RowsRest} -> search_best_impl(NewWordsUsed, Rest, Recognized, RowsRest);
+        false -> false
+    end.
+
+create_search_result(false, false) -> false;
+create_search_result({true, Module, Func}, false) -> {true, Module, Func, []};
+create_search_result(false, {incomplete, Rows}) -> {incomplete, Rows};
 create_search_result({true, Module, Func}, {incomplete, Rows}) -> {true, Module, Func, Rows}.
 
-search_incomplete_match(Words, SearchTable) ->
-    Result = lists:filter(fun({Row, _Module, _Func}) -> compare_words(Words, Row) == incomplete end, SearchTable),
+search_incomplete_match(Words, Table) ->
+    Result = lists:filter(fun({Cond, _Module, _Func}) -> compare_words(Words, Cond) == incomplete end, Table),
     case Result of
-        [] -> {false, not_found};
+        [] -> false;
         Other -> {incomplete, Other}
     end.
 
-search_full_match(Words, SearchTable) ->
-    Result = lists:filter(fun({Row, _Module, _Func}) -> compare_words(Words, Row) == true end, SearchTable),
+search_full_match(Words, Table) ->
+    Result = lists:filter(fun({Cond, _Module, _Func}) -> compare_words(Words, Cond) == true end, Table),
     case Result of
-        [] -> {false, not_found};
-        [{_SearchRow, Module, Func}] -> {true, Module, Func}
+        [] -> false;
+        [{_Cond, Module, Func}] -> {true, Module, Func}
     end.
 
 compare_words([], []) -> true;
 compare_words(_WordsRest, []) -> false;
-compare_words([], _SearchRowRest) -> incomplete;
-compare_words([LeftWord | WordsRest], [{RightWord, MinPrefixLength} | SearchRowRest]) ->
+compare_words([], _CondRest) -> incomplete;
+compare_words([LeftWord | WordsRest], [{RightWord, MinPrefixLength} | CondRest]) ->
     CompareResult = lists:prefix(LeftWord, RightWord) andalso length(LeftWord) >= MinPrefixLength,
     case CompareResult of
-        true -> compare_words(WordsRest, SearchRowRest);
+        true -> compare_words(WordsRest, CondRest);
         false -> false
     end.
