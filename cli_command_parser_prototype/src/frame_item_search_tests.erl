2266bc6444ae385dabcb533b31c3e8d14b918d36
Ushakov Andrey <std_string@mail.ru>
2014-11-11 21:05:57 +0500

    refactoring

--- cli_command_parser_prototype/src/frame_item_search_tests.erl
+++ cli_command_parser_prototype/src/frame_item_search_tests.erl
@@ -6,24 +6,6 @@
 
 -include("common_defs.hrl").
 
--define(PING_MODULE, ping_impl).
--define(CONF_TERM_MODULE, configure_terminal_impl).
--define(LOGIN_MODULE, login_impl).
--define(LOGOUT_MODULE, logout_impl).
--define(INTERFACE_MODULE, interface_impl).
--define(IFRANGE_MODULE, interface_range_impl).
--define(VLAN_MODULE, vlan_impl).
--define(NOVLAN_MODULE, novlan_impl).
--define(SWACCESS_VLAN_MODULE, swaccess_vlan_impl).
--define(NOSWACCESS_VLAN_MODULE, noswaccess_vlan_impl).
--define(NAME_MODULE, name_impl).
--define(NONAME_MODULE, noname_impl).
--define(END_MODULE, end_impl).
--define(EXIT_MODULE, exit_impl).
--define(SHOW_VLAN_MODULE, show_vlan_impl).
-
--define(FUNCTION, execute).
-
 -define(WORD(Value), #frame_item{type = word, value = Value}).
 -define(STRING(Value), #frame_item{type = string, value = Value}).
 -define(FRAME_ITEM(Type, Value), #frame_item{type = Type, value = Value}).
--- cli_command_parser_prototype/src/name_search_tests.erl
+++ cli_command_parser_prototype/src/name_search_tests.erl
@@ -4,23 +4,7 @@
 
 -include_lib("eunit/include/eunit.hrl").
 
--define(PING_MODULE, ping_impl).
--define(CONF_TERM_MODULE, configure_terminal_impl).
--define(LOGIN_MODULE, login_impl).
--define(LOGOUT_MODULE, logout_impl).
--define(INTERFACE_MODULE, interface_impl).
--define(IFRANGE_MODULE, interface_range_impl).
--define(VLAN_MODULE, vlan_impl).
--define(NOVLAN_MODULE, novlan_impl).
--define(SWACCESS_VLAN_MODULE, swaccess_vlan_impl).
--define(NOSWACCESS_VLAN_MODULE, noswaccess_vlan_impl).
--define(NAME_MODULE, name_impl).
--define(NONAME_MODULE, noname_impl).
--define(END_MODULE, end_impl).
--define(EXIT_MODULE, exit_impl).
--define(SHOW_VLAN_MODULE, show_vlan_impl).
-
--define(FUNCTION, execute).
+-include("common_defs.hrl").
 
 %% ====================================================================
 %% Test functions
