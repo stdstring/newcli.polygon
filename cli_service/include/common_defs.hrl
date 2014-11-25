530f74b1b5682b1a27dc2151fdf8ffc35d3dbbdc
Ushakov Andrey <std_string@mail.ru>
2014-11-25 21:48:43 +0500

    continue fill cli_service with code from cli_frontend

--- cli_service/include/common_defs.hrl
+++ cli_service/include/common_defs.hrl
@@ -1,4 +1,7 @@
 %% common definitions
 
--record(cli_terminal_config, {}).
+-define(LISTEN_SUPERVISOR_NAME, cli_terminal_listen_supervisor).
+-define(LISTEN_ENDPOINT_NAME, cli_terminal_listen_endpoint).
+
+-record(cli_terminal_config, {port_number = 65535 :: 1..65535, max_client_count = 10 :: pos_integer()}).
 -record(global_config, {cli_terminal = #cli_terminal_config{} :: #cli_terminal_config{}}).
