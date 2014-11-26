65f0e831ba5de6bea790e2f844cd6eb05f7922b1
Ushakov Andrey <std_string@mail.ru>
2014-11-26 22:11:45 +0500

    finish command_parser

--- cli_command_parser/src/command_parser.erl
+++ cli_command_parser/src/command_parser.erl
@@ -2,18 +2,29 @@
 
 -module(command_parser).
 
--export([process/2]).
+-export([process/3]).
 
+-include("frame_defs.hrl").
 -include("lexical_defs.hrl").
+-include("name_search_defs.hrl").
+-include("syntax_defs.hrl").
 
 %% ====================================================================
 %% API functions
 %% ====================================================================
 
 -spec process(Source :: string(),
-              LexConfig :: #lex_analyzer_config{}) -> 'ok'.
-process(_Source, _LexConfig) ->
-    ok.
+              LexConfig :: #lex_analyzer_config{},
+              SyntaxConfig :: #syntax_analyzer_config{}) -> 'ok'.
+process(Source, LexConfig, SyntaxConfig) ->
+    case lex_analyzer:process(Source, LexConfig) of
+        {true, TokenList} ->
+            case syntax_analyzer:process(TokenList, SyntaxConfig) of
+                {true, Result} -> {true, Result};
+                {false, Reason} -> {false, Reason}
+            end;
+        {false, Reason} -> {false, Reason}
+    end.
 
 %% ====================================================================
 %% Internal functions
