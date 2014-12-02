e2b121bb44946058ce85f05c7827bff2ef8dea8a
Ushakov Andrey <std_string@mail.ru>
2014-12-02 21:52:21 +0500

    start work on command's factory

---
+++ cli_service/src/command/command_factory.erl
@@ -0,0 +1,19 @@
+%% @author std-string
+
+-module(command_factory).
+
+-export([process/3]).
+
+%% ====================================================================
+%% API functions
+%% ====================================================================
+
+process(Source, LexConfig, SyntaxConfig) ->
+    case command_parser:process(Source, LexConfig, SyntaxConfig) of
+        {true, Result} -> ;
+        {false, Reason} ->
+    end.
+
+%% ====================================================================
+%% Internal functions
+%% ====================================================================
