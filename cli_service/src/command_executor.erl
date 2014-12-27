c6993a896d5821220fa5366e93d0c0a2a4027ade
Ushakov Andrey <std_string@mail.ru>
2014-12-27 21:28:38 +0500

    fix command_executor

--- cli_service/src/command_executor.erl
+++ cli_service/src/command_executor.erl
@@ -24,47 +24,25 @@ process({?COMMAND_ERROR, Error}, State) ->
     State;
 process({?FINISH_COMMAND, _ReturnCode, _ExecutionState}, State) ->
     State;
-process({?FINISH_EXEC, 0, _ExecutionState}, State) ->
-    NewState = State#client_handler_state{},
-    NewState;
-%%process({command_end, ExecutionState, ReturnCode}, State) ->
-%%    Error = message_helper:format(?EXEC_FAILED, [ReturnCode]),
-%%    NewState = State#client_handler_state{execution_state = ExecutionState, command_chain = []},
-%%    command_helper:send_error(NewState, Error),
-%%    command_helper:send_end(NewState),
-%%    NewState;
-process({?FINISH_EXEC, ReturnCode, _ExecutionState}, State) ->
-    Error = message_helper:format(?EXEC_FAILED, [ReturnCode]),
-    NewState = State#client_handler_state{},
+process({?FINISH_EXEC, 0, ExecutionState}, State) ->
+    User = list_utils:get_value_by_key_with_default(ExecutionState, ?USER_KEY, 1, undefined),
+    State#client_handler_state{user = User, current_command = undefined};
+process({?FINISH_EXEC, ReturnCode, ExecutionState}, State) ->
+    Error = string_utils:format(?EXEC_FAILED, [ReturnCode]),
+    User = list_utils:get_value_by_key_with_default(ExecutionState, ?USER_KEY, 1, undefined),
+    NewState = State#client_handler_state{user = User, current_command = undefined},
     command_helper:send_error(NewState, Error),
     command_helper:send_end(NewState),
     NewState;
-%%process(interrupt_command, #client_handler_state{current_command = undefined} = State) ->
-%%    State;
-%%process(interrupt_command, State) ->
-%%    Command = State#client_handler_state.current_command,
-%%    exit(Command, interrupt),
-%%    NewState = State#client_handler_state{command_chain = [], current_command = undefined},
-%%    command_helper:send_end(NewState),
-%%    NewState.
+process(?INTERRUPT, #client_handler_state{current_command = undefined} = State) ->
+    State;
 process(?INTERRUPT, State) ->
-    NewState = State#client_handler_state{},
+    Command = State#client_handler_state.current_command,
+    exit(Command, interrupt),
+    NewState = State#client_handler_state{current_command = undefined},
     command_helper:send_end(NewState),
     NewState.
 
 %% ====================================================================
 %% Internal functions
 %% ====================================================================
-
-%%-spec process_next_command(State :: #client_handler_state{}) -> #client_handler_state{}.
-%%process_next_command(State) ->
-%%    case State#client_handler_state.command_chain of
-%%        [] ->
-%%            command_helper:send_end(State),
-%%            State#client_handler_state{command_chain = [], current_command = undefined};
-%%        [#command_entry{module = Module, command_line_rest = CommandLineRest} | Rest] ->
-%%            ClientHandler = self(),
-%%            ExecutionState = State#client_handler_state.execution_state,
-%%            Command = apply(Module, execute, [CommandLineRest, ClientHandler, ExecutionState]),
-%%            State#client_handler_state{command_chain = Rest, current_command = Command}
-%%    end.
