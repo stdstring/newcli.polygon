ea6005862a4f3e25702c232b19919a1836eb07b5
Ushakov Andrey <std_string@mail.ru>
2015-01-09 22:34:33 +0500

    start create common commands

---
+++ cli_service/src/command_common/login_command.erl
@@ -0,0 +1,24 @@
+%% @author std-string
+
+-module(login_command).
+
+-behaviour(command_behaviour).
+
+-export([get_name/0, get_command_body/0, get_help/0, execute/4]).
+
+%% ====================================================================
+%% API functions
+%% ====================================================================
+
+get_name() -> login.
+
+get_command_body() -> ["login"].
+
+get_help() -> "login help".
+
+execute(_Args, _Stdout, _Stderr, _ExecContext) ->
+    throw(enotimpl).
+
+%% ====================================================================
+%% Internal functions
+%% ====================================================================
