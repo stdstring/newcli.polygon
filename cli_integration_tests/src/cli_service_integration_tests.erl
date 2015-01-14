bee2f8eddb37cd9beb72c86c16a36265863f6c02
Ushakov Andrey <std_string@mail.ru>
2015-01-14 22:16:13 +0500

    some fixes

--- cli_integration_tests/src/cli_service_integration_tests.erl
+++ cli_integration_tests/src/cli_service_integration_tests.erl
@@ -15,27 +15,27 @@ sample_test() ->
     Service = integration_tests_common:start_cli_service(),
     integration_tests_common:stop_cli_service(Service).
 
-%%integration_test_() ->
-%%    Tests = [{"interaction with service", fun check_service/0}],
-%%    {foreach, fun integration_tests_common:setup/0, fun integration_common_manager:cleanup/1, Tests}.
+integration_test_() ->
+    Tests = [{"interaction with service", fun check_service/0}],
+    {foreach, fun integration_tests_common:start_cli_service/0, fun integration_tests_common:stop_cli_service/1, Tests}.
 
 %% ====================================================================
 %% Internal functions
 %% ====================================================================
 
-%%-spec check_service() -> 'ok'.
-%%check_service() ->
-%%    {OutputResponse, EndResponse} = interact_with_service(),
-%%    io:format(user, "Output = ~p~n", [OutputResponse]),
-%%    io:format(user, "End = ~p~n", [EndResponse]),
-%%    ok.
+-spec check_service() -> 'ok'.
+check_service() ->
+    {OutputResponse, EndResponse} = interact_with_service(),
+    io:format(user, "Output = ~p~n", [OutputResponse]),
+    io:format(user, "End = ~p~n", [EndResponse]),
+    ok.
 
-%%-spec interact_with_service() -> {OutputResponse :: {'command_out', CommandOut :: string()}, EndResponse :: {'end', Prompt :: string()}}.
-%%interact_with_service() ->
-%%    {ok, Socket} = gen_tcp:connect(?SERVICE_ENDPOINT_ADDRESS, ?SERVICE_ENDPOINT_PORT, [binary, {packet, 4}, {active, false}]),
-%%    gen_tcp:send(Socket, term_to_binary(?COMMAND)),
-%%    {ok, OutputPacket} = gen_tcp:recv(Socket, 0),
-%%    OutputResponse = binary_to_term(OutputPacket),
-%%    {ok, EndPacket} = gen_tcp:recv(Socket, 0),
-%%    EndResponse = binary_to_term(EndPacket),
-%%    {OutputResponse, EndResponse}.
+-spec interact_with_service() -> {OutputResponse :: {'command_out', CommandOut :: string()}, EndResponse :: {'end', Prompt :: string()}}.
+interact_with_service() ->
+    {ok, Socket} = gen_tcp:connect(?SERVICE_ENDPOINT_ADDRESS, ?SERVICE_ENDPOINT_PORT, [binary, {packet, 4}, {active, false}]),
+    gen_tcp:send(Socket, term_to_binary(?COMMAND)),
+    {ok, OutputPacket} = gen_tcp:recv(Socket, 0),
+    OutputResponse = binary_to_term(OutputPacket),
+    {ok, EndPacket} = gen_tcp:recv(Socket, 0),
+    EndResponse = binary_to_term(EndPacket),
+    {OutputResponse, EndResponse}.
