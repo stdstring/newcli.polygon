85edc251777f10f36447b2d2098355f94056c4c5
Ushakov Andrey <std_string@mail.ru>
2015-01-26 22:33:24 +0500

    add additional functionality for search frame items (specially for help command) ++ test on it

--- cli_command_parser/src/frame_item_search.erl
+++ cli_command_parser/src/frame_item_search.erl
@@ -2,7 +2,7 @@
 
 -module(frame_item_search).
 
--export([search_best/2]).
+-export([search_best/2, search_suitable/2, search_exact/2]).
 
 -include("frame_defs.hrl").
 -include("name_search_defs.hrl").
@@ -16,9 +16,57 @@
 search_best(FrameItems, NameTable) ->
     search_best_impl([], FrameItems, undefined, NameTable).
 
+-spec search_suitable(FrameItems :: [#frame_item{}], NameTable :: name_search_table()) -> [atom()].
+search_suitable(FrameItems, NameTable) ->
+    case transform_frame_items(FrameItems) of
+        {true, Values} -> search_suitable_impl(Values, NameTable);
+        false -> []
+    end.
+
+-spec search_exact(FrameItems :: [#frame_item{}], NameTable :: name_search_table()) ->
+    {'true', Module :: atom()} | 'false'.
+search_exact(FrameItems, NameTable) ->
+    case transform_frame_items(FrameItems) of
+        {true, Values} -> search_exact_impl(Values, NameTable);
+        false -> false
+    end.
+
 %% ====================================================================
 %% Internal functions
 %% ====================================================================
+
+-spec transform_frame_items(FrameItems :: [#frame_item{}]) -> {'true', Words :: [string()]} | false.
+transform_frame_items(FrameItems) ->
+    transform_frame_items(FrameItems, []).
+
+-spec transform_frame_items(FrameItems :: [#frame_item{}], Result :: [string()]) ->
+     {'true', Words :: [string()]} | false.
+transform_frame_items([], Result) ->
+    {true, lists:reverse(Result)};
+transform_frame_items([#frame_item{type = word, value = Word} | Rest], Result) ->
+    transform_frame_items(Rest, [Word] ++ Result);
+transform_frame_items([#frame_item{type = string, value = Word} | Rest], Result) ->
+    transform_frame_items(Rest, [Word] ++ Result);
+transform_frame_items([#frame_item{} | _Rest], _Result) ->
+    false.
+
+-spec search_suitable_impl(Values :: [string()], NameTable :: name_search_table()) -> [atom()].
+search_suitable_impl(Values, NameTable) ->
+    case name_search:search(Values, NameTable) of
+        {true, Module, RowsRest} ->
+            [Module] ++ lists:map(fun({_SearchItem, Value}) -> Value end, RowsRest);
+        {incomplete, RowsRest} ->
+            lists:map(fun({_SearchItem, Value}) -> Value end, RowsRest);
+        false -> []
+    end.
+
+-spec search_exact_impl(Values :: [string()], NameTable :: name_search_table()) ->
+    {'true', Module :: atom()} | 'false'.
+search_exact_impl(Values, NameTable) ->
+    case name_search:search(Values, NameTable) of
+        {true, Module, _RowsRest} -> {true, Module};
+        _Other -> false
+    end.
 
 -spec search_best_impl(WordsUsed :: [string()],
                       FrameItems :: [#frame_item{}],
@@ -30,24 +78,38 @@ search_best_impl(_WordsUsed, [], {RecModule, RecRest}, _Rows) -> {true, RecModul
 search_best_impl(_WordsUsed, _ItemsRest, undefined, []) -> false;
 search_best_impl(_WordsUsed, _ItemsRest, {RecModule, RecRest}, []) -> {true, RecModule, RecRest};
 search_best_impl(WordsUsed, [#frame_item{type = word, value = Word} | Rest], undefined, Rows) ->
-    NewWordsUsed = WordsUsed ++ [Word],
-    case name_search:search(NewWordsUsed, Rows) of
-        {true, Module, RowsRest} ->
-            NewRecognized = {Module, Rest},
-            search_best_impl(NewWordsUsed, Rest, NewRecognized, RowsRest);
-        {incomplete, RowsRest} -> search_best_impl(NewWordsUsed, Rest, undefined, RowsRest);
-        false -> false
-    end;
+    search_best_process_word(WordsUsed, Word, Rest, undefined, Rows);
 search_best_impl(WordsUsed, [#frame_item{type = word, value = Word} | Rest], {RecModule, RecRest}, Rows) ->
-    NewWordsUsed = WordsUsed ++ [Word],
-    case name_search:search(NewWordsUsed, Rows) of
-        {true, Module, RowsRest} ->
-            NewRecognized = {Module, Rest},
-            search_best_impl(NewWordsUsed, Rest, NewRecognized, RowsRest);
-        {incomplete, RowsRest} -> search_best_impl(NewWordsUsed, Rest, {RecModule, RecRest}, RowsRest);
-        false -> {true, RecModule, RecRest}
-    end;
+    search_best_process_word(WordsUsed, Word, Rest, {RecModule, RecRest}, Rows);
+search_best_impl(WordsUsed, [#frame_item{type = string, value = Word} | Rest], undefined, Rows) ->
+    search_best_process_word(WordsUsed, Word, Rest, undefined, Rows);
+search_best_impl(WordsUsed, [#frame_item{type = string, value = Word} | Rest], {RecModule, RecRest}, Rows) ->
+    search_best_process_word(WordsUsed, Word, Rest, {RecModule, RecRest}, Rows);
 search_best_impl(_WordsUsed, [#frame_item{} | _Rest], undefined, _Rows) ->
     false;
 search_best_impl(_WordsUsed, [#frame_item{} | _Rest], {RecModule, RecRest}, _Rows) ->
     {true, RecModule, RecRest}.
+
+-spec search_best_process_word(WordsUsed :: [string()],
+                               Word :: string(),
+                               WordsRest :: [#frame_item{}],
+                               SearchResult :: {Module :: atom(), Rest :: [#frame_item{}]} | 'undefined',
+                               Rows :: name_search_table()) -> no_return() | 'false'.
+search_best_process_word(WordsUsed, Word, WordsRest, undefined, Rows) ->
+    NewWordsUsed = WordsUsed ++ [Word],
+    case name_search:search(NewWordsUsed, Rows) of
+        {true, Module, RowsRest} ->
+            NewRecognized = {Module, WordsRest},
+            search_best_impl(NewWordsUsed, WordsRest, NewRecognized, RowsRest);
+        {incomplete, RowsRest} -> search_best_impl(NewWordsUsed, WordsRest, undefined, RowsRest);
+        false -> false
+    end;
+search_best_process_word(WordsUsed, Word, WordsRest, {RecModule, RecRest}, Rows) ->
+    NewWordsUsed = WordsUsed ++ [Word],
+    case name_search:search(NewWordsUsed, Rows) of
+        {true, Module, RowsRest} ->
+            NewRecognized = {Module, WordsRest},
+            search_best_impl(NewWordsUsed, WordsRest, NewRecognized, RowsRest);
+        {incomplete, RowsRest} -> search_best_impl(NewWordsUsed, WordsRest, {RecModule, RecRest}, RowsRest);
+        false -> {true, RecModule, RecRest}
+    end.
--- cli_command_parser_tests/src/frame_item_search_tests.erl
+++ cli_command_parser_tests/src/frame_item_search_tests.erl
@@ -17,34 +17,69 @@
 
 search_best_test_() ->
     NameTable = name_search_config:create(),
-    [success_check("search ping XXX", ?PING_MODULE, [?WORD("XXX")], [?WORD("ping"), ?WORD("XXX")], NameTable),
-     success_check("search p XXX", ?PING_MODULE, [?WORD("XXX")], [?WORD("p"), ?WORD("XXX")], NameTable),
-     fail_check("search png XXX", [?WORD("png"), ?WORD("XXX")], NameTable),
-     success_check("search ping \"XXX\"", ?PING_MODULE, [?STRING("XXX")], [?WORD("ping"), ?STRING("XXX")], NameTable),
-     fail_check("search \"ping\" XXX", [?STRING("ping"), ?WORD("XXX")], NameTable),
-     success_check("search configure terminal", ?CONF_TERM_MODULE, [], [?WORD("configure"), ?WORD("terminal")], NameTable),
-     success_check("search c terminal", ?CONF_TERM_MODULE, [], [?WORD("c"), ?WORD("terminal")], NameTable),
-     success_check("search c t", ?CONF_TERM_MODULE, [], [?WORD("c"), ?WORD("t")], NameTable),
-     success_check("search interface 666", ?INTERFACE_MODULE, [?WORD("666")], [?WORD("interface"), ?WORD("666")], NameTable),
-     success_check("search i 666", ?INTERFACE_MODULE, [?WORD("666")], [?WORD("i"), ?WORD("666")], NameTable),
-     success_check("search interface range 666", ?IFRANGE_MODULE, [?WORD("666")], [?WORD("interface"), ?WORD("range"), ?WORD("666")], NameTable),
-     success_check("search i range 666", ?IFRANGE_MODULE, [?WORD("666")], [?WORD("i"), ?WORD("range"), ?WORD("666")], NameTable),
-     success_check("search interface r 666", ?IFRANGE_MODULE, [?WORD("666")], [?WORD("interface"), ?WORD("r"), ?WORD("666")], NameTable),
-     success_check("search i r 666", ?IFRANGE_MODULE, [?WORD("666")], [?WORD("i"), ?WORD("r"), ?WORD("666")], NameTable),
-     success_check("search name IDDQD", ?NAME_MODULE, [?WORD("IDDQD")], [?WORD("name"), ?WORD("IDDQD")], NameTable),
-     success_check("search na IDDQD", ?NAME_MODULE, [?WORD("IDDQD")], [?WORD("na"), ?WORD("IDDQD")], NameTable),
-     fail_check("search n IDDQD", [?WORD("n"), ?WORD("IDDQD")], NameTable),
-     success_check("search no name", ?NONAME_MODULE, [], [?WORD("no"), ?WORD("name")], NameTable),
-     fail_check("search n name", [?WORD("n"), ?WORD("name")], NameTable),
-     fail_check("search unknown_command IDDQD", [?WORD("unknown_command"), ?WORD("IDDQD")], NameTable),
-     fail_check("search 666 IDDQD", [?FRAME_ITEM(integer, 666), ?WORD("IDDQD")], NameTable)].
+    [success_search_best_check("search ping XXX", ?PING_MODULE, [?WORD("XXX")], [?WORD("ping"), ?WORD("XXX")], NameTable),
+     success_search_best_check("search p XXX", ?PING_MODULE, [?WORD("XXX")], [?WORD("p"), ?WORD("XXX")], NameTable),
+     fail_search_best_check("search png XXX", [?WORD("png"), ?WORD("XXX")], NameTable),
+     success_search_best_check("search ping \"XXX\"", ?PING_MODULE, [?STRING("XXX")], [?WORD("ping"), ?STRING("XXX")], NameTable),
+     success_search_best_check("search \"ping\" XXX", ?PING_MODULE, [?WORD("XXX")], [?STRING("ping"), ?WORD("XXX")], NameTable),
+     fail_search_best_check("search \"ping XXX\"", [?STRING("ping XXX")], NameTable),
+     success_search_best_check("search configure terminal", ?CONF_TERM_MODULE, [], [?WORD("configure"), ?WORD("terminal")], NameTable),
+     success_search_best_check("search c terminal", ?CONF_TERM_MODULE, [], [?WORD("c"), ?WORD("terminal")], NameTable),
+     success_search_best_check("search c t", ?CONF_TERM_MODULE, [], [?WORD("c"), ?WORD("t")], NameTable),
+     success_search_best_check("search interface 666", ?INTERFACE_MODULE, [?WORD("666")], [?WORD("interface"), ?WORD("666")], NameTable),
+     success_search_best_check("search i 666", ?INTERFACE_MODULE, [?WORD("666")], [?WORD("i"), ?WORD("666")], NameTable),
+     success_search_best_check("search interface range 666", ?IFRANGE_MODULE, [?WORD("666")], [?WORD("interface"), ?WORD("range"), ?WORD("666")], NameTable),
+     success_search_best_check("search i range 666", ?IFRANGE_MODULE, [?WORD("666")], [?WORD("i"), ?WORD("range"), ?WORD("666")], NameTable),
+     success_search_best_check("search interface r 666", ?IFRANGE_MODULE, [?WORD("666")], [?WORD("interface"), ?WORD("r"), ?WORD("666")], NameTable),
+     success_search_best_check("search i r 666", ?IFRANGE_MODULE, [?WORD("666")], [?WORD("i"), ?WORD("r"), ?WORD("666")], NameTable),
+     success_search_best_check("search name IDDQD", ?NAME_MODULE, [?WORD("IDDQD")], [?WORD("name"), ?WORD("IDDQD")], NameTable),
+     success_search_best_check("search na IDDQD", ?NAME_MODULE, [?WORD("IDDQD")], [?WORD("na"), ?WORD("IDDQD")], NameTable),
+     fail_search_best_check("search n IDDQD", [?WORD("n"), ?WORD("IDDQD")], NameTable),
+     success_search_best_check("search no name", ?NONAME_MODULE, [], [?WORD("no"), ?WORD("name")], NameTable),
+     fail_search_best_check("search n name", [?WORD("n"), ?WORD("name")], NameTable),
+     fail_search_best_check("search unknown_command IDDQD", [?WORD("unknown_command"), ?WORD("IDDQD")], NameTable),
+     fail_search_best_check("search 666 IDDQD", [?FRAME_ITEM(integer, 666), ?WORD("IDDQD")], NameTable),
+     fail_search_best_check("empty search", [], NameTable)].
+
+search_suitable_test_() ->
+    NameTable = name_search_config:create(),
+    [search_suitable_check("search ping", [?PING_MODULE], [?WORD("ping")], NameTable),
+     search_suitable_check("search p", [?PING_MODULE], [?WORD("p")], NameTable),
+     search_suitable_check("search pong", [], [?WORD("pong")], NameTable),
+     search_suitable_check("search interface", [?INTERFACE_MODULE, ?IFRANGE_MODULE], [?WORD("interface")], NameTable),
+     search_suitable_check("search i", [?INTERFACE_MODULE, ?IFRANGE_MODULE], [?WORD("i")], NameTable),
+     search_suitable_check("search interface range", [?IFRANGE_MODULE], [?WORD("interface"), ?WORD("range")], NameTable),
+     search_suitable_check("search interface r", [?IFRANGE_MODULE], [?WORD("interface"), ?WORD("r")], NameTable),
+     search_suitable_check("search no", [?NOVLAN_MODULE, ?NOSWACCESS_VLAN_MODULE, ?NONAME_MODULE], [?WORD("no")], NameTable),
+     search_suitable_check("search \"ping\"", [?PING_MODULE], [?STRING("ping")], NameTable),
+     search_suitable_check("search ping with other type", [], [?FRAME_ITEM(some_type, "ping")], NameTable)].
+
+search_exact_test_() ->
+    NameTable = name_search_config:create(),
+    [success_search_exact_check("search ping", ?PING_MODULE, [?WORD("ping")], NameTable),
+     success_search_exact_check("search p", ?PING_MODULE, [?WORD("p")], NameTable),
+     success_search_exact_check("search interface", ?INTERFACE_MODULE, [?WORD("interface")], NameTable),
+     success_search_exact_check("search i", ?INTERFACE_MODULE, [?WORD("i")], NameTable),
+     fail_search_exact_check("search pong", [?WORD("pong")], NameTable),
+     fail_search_exact_check("search no", [?WORD("no")], NameTable),
+     success_search_exact_check("search \"ping\"", ?PING_MODULE, [?STRING("ping")], NameTable),
+     fail_search_exact_check("search ping with other type", [?FRAME_ITEM(some_type, "ping")], NameTable)].
 
 %% ====================================================================
 %% Internal functions
 %% ====================================================================
 
-success_check(Description, Module, Args, Items, NameTable) ->
+success_search_best_check(Description, Module, Args, Items, NameTable) ->
     {Description, ?_assertEqual({true, Module, Args}, frame_item_search:search_best(Items, NameTable))}.
 
-fail_check(Description, Items, NameTable) ->
+fail_search_best_check(Description, Items, NameTable) ->
     {Description, ?_assertEqual(false, frame_item_search:search_best(Items, NameTable))}.
+
+search_suitable_check(Description, Expected, Items, NameTable) ->
+    {Description, ?_assertEqual(Expected, frame_item_search:search_suitable(Items, NameTable))}.
+
+success_search_exact_check(Description, Module, Items, NameTable) ->
+    {Description, ?_assertEqual({true, Module}, frame_item_search:search_exact(Items, NameTable))}.
+
+fail_search_exact_check(Description, Items, NameTable) ->
+    {Description, ?_assertEqual(false, frame_item_search:search_exact(Items, NameTable))}.
