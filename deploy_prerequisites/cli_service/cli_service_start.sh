5d3a55767bc0d7fcc58cf849e5e77ae6fec8b6da
Ushakov Andrey <std_string@mail.ru>
2015-03-17 22:13:03 +0500

    fix prerequisites for deploy

--- deploy_prerequisites/cli_service/cli_service_start.sh
+++ deploy_prerequisites/cli_service/cli_service_start.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
--erl -noshell -pa /usr/local/lib/cli_service -eval "application:start(cli_service_application)"
+erl -noshell -pa /usr/local/lib/cli_service -eval "application:start(cli_service_application)"
